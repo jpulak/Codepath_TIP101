0f0989153967e21f593f66724a72d20b2c678955
Jana Pulak <jpulak@gmail.com>
2026-06-04 18:16:43 -0500

    s2-1

--- Unit 1/Session 2/V1 Problem Set/prob1.py
+++ Unit 1/Session 2/V1 Problem Set/prob1.py
@@ -1,12 +1,21 @@
 #Problem 1: Print List
-'''Write a function print_list() that takes in a list lst as a parameter and prints out each item in the list.
+'''Write a function print_list() that takes in a list lst as 
+a parameter and prints out each item in the list.
 
 def print_list(lst):
     pass
-Example Input: lst = ["squirtle", "gengar", "charizard", "pikachu"]
+Example Input:
+ lst = ["squirtle", "gengar", "charizard", "pikachu"]
 Example Output:
 
 squirtle
 gengar
 charizard
 pikachu'''
+
+def print_list(lst):
+    for word in lst:
+        print(word)
+
+lst = ["squirtle", "gengar", "charizard", "pikachu"]
+print_list(lst)
