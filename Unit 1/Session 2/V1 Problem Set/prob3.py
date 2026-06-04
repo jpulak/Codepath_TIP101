d962a8a4cd705aeed1e2635208c177acf0b6396a
Jana Pulak <jpulak@gmail.com>
2026-06-04 18:25:36 -0500

    s2-3

--- Unit 1/Session 2/V1 Problem Set/prob3.py
+++ Unit 1/Session 2/V1 Problem Set/prob3.py
@@ -1,5 +1,7 @@
 #Problem 3: Return Doubled List
-'''Modify the function doubled() so that instead of printing the items, it returns a new list of the doubled numbers.
+'''Modify the function doubled() so that 
+instead of printing the items, it returns a new 
+list of the doubled numbers.
 
 Example Usage:
 
@@ -9,3 +11,13 @@ print(new_lst)
 Example Output:
 
 [2, 4, 6]'''
+
+def doubled(lst):
+    new=[]
+    for num in lst:
+        new.append(num*2)
+    return new
+
+lst = [1,2,3]
+new_lst = doubled(lst)
+print(new_lst)
