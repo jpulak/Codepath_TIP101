34c92c3a09204022e30f860d672077e8656a2a4b
Jana Pulak <jpulak@gmail.com>
2026-06-04 22:21:59 -0500

    s2-4

--- Unit 1/Session 2/V1 Problem Set/prob4.py
+++ Unit 1/Session 2/V1 Problem Set/prob4.py
@@ -1,5 +1,8 @@
 #Problem 4: Flip Signs
-'''Write a function flip_sign() that takes in a list of integers lst as a parameter and returns a new list where each number in the original list has been multiplied by -1.
+'''Write a function flip_sign() that takes in a
+ list of integers lst as a parameter and returns a 
+ new list where each number in the original list has
+   been multiplied by -1.
 
 def flip_sign(lst):
     pass
@@ -12,3 +15,13 @@ Example Output:
 
 [-1, 2, 3, -4]
 '''
+
+def flip_sign(lst):
+    new=[]
+    for i in lst:
+        new.append(i*-1)
+    return new
+    
+lst = [1,-2,-3,4]
+flipped_lst = flip_sign(lst)
+print(flipped_lst)
