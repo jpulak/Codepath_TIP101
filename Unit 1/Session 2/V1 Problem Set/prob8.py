bf03cf3c00fdffd4b44e23784dbf614340d23873
Jana Pulak <jpulak@gmail.com>
2026-06-05 02:07:14 -0500

    s2-8

--- Unit 1/Session 2/V1 Problem Set/prob8.py
+++ Unit 1/Session 2/V1 Problem Set/prob8.py
@@ -1,5 +1,6 @@
 #Problem 8: Multiples of Five
-'''Write a function multiples_of_five() that prints out multiples of 5 between 1 and 100 (inclusive).
+'''Write a function multiples_of_five() 
+that prints out multiples of 5 between 1 and 100 (inclusive).
 
 def multiples_of_five():
     pass
@@ -15,3 +16,14 @@ Example Output:
 95
 100
 '''
+
+def multiples_of_five():
+    for i in range(5,101,5):
+        print(i)
+
+    # for num in range(5,101):
+    #     if num%5 ==0:
+    #         print(num)
+
+
+print(multiples_of_five())
