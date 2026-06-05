4e12cc56547c8eaa00b9ee2e1a7e0b1cfc781342
Jana Pulak <jpulak@gmail.com>
2026-06-05 02:54:44 -0500

    s2-9

--- Unit 1/Session 2/V2 Problem Set/prob9.py
+++ Unit 1/Session 2/V2 Problem Set/prob9.py
@@ -1,5 +1,8 @@
 #Problem 9: Create Number
-'''Write a function list_to_number() that takes in a list digits where each item is a digit between 0-9. The function returns the number they represent when combined.
+'''Write a function list_to_number() that takes 
+in a list digits where each item is a digit between
+ 0-9. The function returns the number they represent
+   when combined.
 
 def list_to_number(digits):
     pass
@@ -9,3 +12,18 @@ digits = [1,0,3]
 number = list_to_number(digits)
 print(number)
 Example Output: 103'''
+
+def list_to_number(digits):
+    return("".join(map(str,digits)))
+
+    # int("".join([str(i) for i in digits]))
+
+# def list_to_number(digits):
+#     number = 0
+#     for digit in digits:
+#         number = number * 10 + digit
+#     return number
+
+digits = [1,0,3]
+number = list_to_number(digits)
+print(number)
