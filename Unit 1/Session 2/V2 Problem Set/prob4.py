d9b2b61617b07fb1aa74a490d1414637e52f7df7
Jana Pulak <jpulak@gmail.com>
2026-06-05 02:40:15 -0500

    s2-4

--- Unit 1/Session 2/V2 Problem Set/prob4.py
+++ Unit 1/Session 2/V2 Problem Set/prob4.py
@@ -1,5 +1,7 @@
 #Problem 4: Check for Number
-'''Write a function check_num() that takes in a list of integers lst and an integer num as parameters and returns True if num is a value in lst and False otherwise.
+'''Write a function check_num() that takes in a list of
+ integers lst and an integer num as parameters and returns
+   True if num is a value in lst and False otherwise.
 
 def check_num(lst, num):
     pass
@@ -14,3 +16,15 @@ Example Output:
 
 True
 False'''
+
+def check_num(lst, num):
+    for i in lst:
+        if i == num:
+            return True
+    return False
+
+lst = [5,2,3,9,10]
+flag1 = check_num(lst,9)
+flag2 = check_num(lst,4)
+print(flag1)
+print(flag2)
