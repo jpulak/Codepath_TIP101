5c462e2dd1501f6e0b4b3b64d7f9f25dc2529669
Jana Pulak <jpulak@gmail.com>
2026-06-05 02:09:42 -0500

    s2-9

--- Unit 1/Session 2/V1 Problem Set/prob9.py
+++ Unit 1/Session 2/V1 Problem Set/prob9.py
@@ -1,5 +1,7 @@
 #Problem 9: Divisors
-'''Write a function find_divisors() that takes in an integer n as a parameter that returns a list of all divisors of n.
+'''Write a function find_divisors() that takes in an 
+integer n as a parameter that returns a list of all 
+divisors of n.
 
 def find_divisors(n):
     pass
@@ -11,3 +13,14 @@ Example Output:
 
 [1, 2, 3, 6]
 '''
+
+def find_divisors(n):
+    all=[]
+    for i in range(1,n+1):
+        if n%i==0:
+            all.append(i)
+
+    return all
+
+lst = find_divisors(6)
+print(lst)
