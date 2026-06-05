50e3d53a4c2e572aff7ec3fd94d9bddecd083ce9
Jana Pulak <jpulak@gmail.com>
2026-06-05 02:35:46 -0500

    s2-2

--- Unit 1/Session 2/V2 Problem Set/prob2.py
+++ Unit 1/Session 2/V2 Problem Set/prob2.py
@@ -1,5 +1,6 @@
 #Problem 2: Average Score
-'''Write a function average() that takes in a list of integers scores as a parameter and returns the average score.
+'''Write a function average() that takes in a list of 
+integers scores as a parameter and returns the average score.
 
 def average(scores):
     pass
@@ -9,3 +10,14 @@ scores = [84,73,92,95,88]
 avg_score = average(scores)
 print(avg_score)
 Example Output: 86.4'''
+
+def average(scores):
+    all=0
+    for num in scores:
+        all +=num
+    
+    return all/len(scores)
+
+scores = [84,73,92,95,88]
+avg_score = average(scores)
+print(avg_score)
