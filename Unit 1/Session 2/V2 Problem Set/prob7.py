96a8123fd93df41c758e177309b9e233cbe17148
Jana Pulak <jpulak@gmail.com>
2026-06-05 02:49:32 -0500

    s2-7

--- Unit 1/Session 2/V2 Problem Set/prob7.py
+++ Unit 1/Session 2/V2 Problem Set/prob7.py
@@ -1,5 +1,7 @@
 #Problem 7: Get Odd Numbers
-'''Write a function get_odds() that takes in a list of integers nums and returns a list of all odd numbers in nums.
+'''Write a function get_odds() that takes 
+in a list of integers nums and returns a list of 
+all odd numbers in nums.
 
 def get_odds(nums):
     pass
@@ -11,3 +13,14 @@ print(odd_nums)
 Example Output:
 
 [5, 1, 5]'''
+
+def get_odds(nums):
+    all=[]
+    for i in nums:
+        if i %2 !=0 :
+            all.append(i)
+    return all
+
+nums = [2,5,1,8,6,5]
+odd_nums = get_odds(nums)
+print(odd_nums)
