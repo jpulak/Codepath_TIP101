dc744b328d06986fa45181594c7e95f3a8d3e0b0
Jana Pulak <jpulak@gmail.com>
2026-06-05 02:15:36 -0500

    s2-10

--- Unit 1/Session 2/V1 Problem Set/prob10.py
+++ Unit 1/Session 2/V1 Problem Set/prob10.py
@@ -1,5 +1,6 @@
 #Problem 10: FizzBuzz
-'''Write a function fizzbuzz() that takes in an integer n as a parameter and prints the numbers from 1 to n.
+'''Write a function fizzbuzz() that takes in an integer n as 
+a parameter and prints the numbers from 1 to n.
 For multiples of 3, print "Fizz" instead of the number.
 For multiples of 5, print "Buzz" instead of the number.
 
@@ -22,3 +23,17 @@ Buzz
 11
 Fizz
 13'''
+
+def fizzbuzz(n):
+    for i in range(1, n+1):
+        if i%3 ==0 and i%5==0:
+            print("FizzBuzz")
+        elif i%3==0:
+            print("Fizz")
+            continue
+        elif i%5 ==0:
+            print("Buzz")
+        else:
+            print(i)
+
+fizzbuzz(20)
