4057ac01aff266fd0d3317f51020bac0615cf730
Jana Pulak <jpulak@gmail.com>
2026-06-04 23:56:50 -0500

    s2-5

--- Unit 1/Session 2/V1 Problem Set/prob5.py
+++ Unit 1/Session 2/V1 Problem Set/prob5.py
@@ -1,5 +1,7 @@
 #Problem 5: Max Difference
-'''Write a function max_difference() that takes in a list of integers lst and returns the difference between the smallest and largest value in the list.
+'''Write a function max_difference() that takes 
+in a list of integers lst and returns the difference 
+between the smallest and largest value in the list.
 
 def max_difference(lst):
     pass
@@ -9,3 +11,20 @@ lst = [5,22,8,10,2]
 max_diff = max_difference(lst)
 print(max_diff)
 Example Output: 20'''
+
+def max_difference(lst):
+
+    max= lst[0]
+    min=lst[0]
+
+    for num in lst:
+        if num>max:
+            max=num
+        if num<min:
+            min=num
+    return max-min
+    # return max(lst)-min(lst)
+
+lst = [5,22,8,10,2]
+max_diff = max_difference(lst)
+print(max_diff)
