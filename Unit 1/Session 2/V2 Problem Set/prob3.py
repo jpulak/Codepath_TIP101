66a03280cad9de4b0afb818b79e5f112c7c9f9d5
Jana Pulak <jpulak@gmail.com>
2026-06-05 02:38:21 -0500

    s2-3

--- Unit 1/Session 2/V2 Problem Set/prob3.py
+++ Unit 1/Session 2/V2 Problem Set/prob3.py
@@ -1,5 +1,7 @@
 #Problem 3: Increment by 1
-'''Write a function increment_values() that takes in a list of integers lst as a parameter and returns a new list containing each element incremented by 1.
+'''Write a function increment_values() that t
+akes in a list of integers lst as a parameter and 
+returns a new list containing each element incremented by 1.
 
 def increment_values(lst):
     pass
@@ -11,3 +13,15 @@ print(new_lst)
 Example Output:
 
 [4, 6, 9, 3]'''
+
+def increment_values(lst):
+    new=[]
+    for num in lst:
+        new.append(num+1)
+
+    return new
+
+lst = [3,5,8,2]
+new_lst = increment_values(lst)
+print(new_lst)
+
