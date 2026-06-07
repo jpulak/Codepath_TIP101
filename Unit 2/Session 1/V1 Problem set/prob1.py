8c197a7a9a0b99f43f8a3cafef8a27d74004ed62
Jana Pulak <jpulak@gmail.com>
2026-06-07 00:57:36 -0500

    s1-1

--- Unit 2/Session 1/V1 Problem set/prob1.py
+++ Unit 2/Session 1/V1 Problem set/prob1.py
@@ -1,5 +1,8 @@
 #Problem 1: All In
-'''Write a function all_in() that takes in a list of integers a and a list of integers b as parameters. Given these two lists, return True if every element in list a is in list b. Return False otherwise.
+'''Write a function all_in() that takes in a list 
+of integers a and a list of integers b as parameters.
+ Given these two lists, return True if every element in 
+ list a is in list b. Return False otherwise.
 
 def all_in(a, b):
     pass
@@ -13,3 +16,14 @@ Example Output:
 
 True
 False'''
+
+def all_in(a, b):
+    for ele in a:
+        if ele not in b:
+            return False
+    return True
+
+lst_1 = [1, 2]
+lst_2 = [1, 2, 3]
+print(all_in(lst_1, lst_2))
+print(all_in(lst_2, lst_1))
