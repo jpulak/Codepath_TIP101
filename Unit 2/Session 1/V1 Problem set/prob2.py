d9991cfcbb4669ffff2b342ab24492096af5ed0a
Jana Pulak <jpulak@gmail.com>
2026-06-08 17:48:23 -0500

    s1-2

--- Unit 2/Session 1/V1 Problem set/prob2.py
+++ Unit 2/Session 1/V1 Problem set/prob2.py
@@ -1,7 +1,17 @@
 #Problem 2: Create a Dictionary
-'''Write a function create_dictionary() that takes in a list of keys and a list of values as parameters. The function returns a dictionary where each item in keys is paired with a corresponding item in values.
+'''Write a function create_dictionary() that takes in a 
+list of 
+keys and a list of values as parameters. 
+The function returns a 
+dictionary where each item in keys is paired with a 
+corresponding
+ item in values.
 
-keys[i] should be paired with values[i] in the dictionary where 0 <= i <= len(keys). You may assume keys and values are the same length.
+keys[i] should be paired with values[i] in the dictionary
+ where 
+0 <= i <= len(keys). You may assume keys and values are the
+ same 
+length.
 
 def create_dictionary(keys, values):
     pass
@@ -11,4 +21,16 @@ keys = ['peanut', 'dragon', 'star', 'pop', 'space']
 values = ['butter', 'fly', 'fish', 'corn', 'ship']
 Example Output:
 
-{'peanut': 'butter', 'dragon': 'fly', 'star': 'fish', 'pop': 'corn', 'space': 'ship'}'''
+{'peanut': 'butter', 'dragon': 'fly', 'star': 'fish', 'pop': 'corn', 
+'space': 'ship'}'''
+
+def create_dictionary(keys, values):
+    all = {}
+    for i in range(len(keys)):
+        all[keys[i]] = values[i]
+    return all
+
+keys = ['peanut', 'dragon', 'star', 'pop', 'space']
+values = ['butter', 'fly', 'fish', 'corn', 'ship']
+        
+print(create_dictionary(keys, values))
