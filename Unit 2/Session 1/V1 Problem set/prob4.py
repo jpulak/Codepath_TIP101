a8d6b37d4882186cf0fb868b25db13a45be89597
Jana Pulak <jpulak@gmail.com>
2026-06-09 00:16:43 -0500

    s1-4

--- Unit 2/Session 1/V1 Problem set/prob4.py
+++ Unit 2/Session 1/V1 Problem set/prob4.py
@@ -1,8 +1,15 @@
 #Problem 4: Keys Versus Values
-'''Write a function keys_v_values() that takes in a dictionary dictionary whose keys and values are both integers. Using at least one loop, the function should find the sum of all keys in the dictionary and the sum of all values.
-If the sum of all keys is greater than the sum of all values, the function should return the string "keys".
-If the sum of all values is greater than the sum of all keys, the function should return the string "values".
-If keys and values have equal sums, the function should return the string "balanced".
+'''Write a function keys_v_values() that takes in a 
+dictionary dictionary whose keys and values are both 
+integers. Using at least one loop, the function should find
+ the sum of all keys in the dictionary and the sum of all 
+ values.
+If the sum of all keys is greater than the sum of all 
+values, the function should return the string "keys".
+If the sum of all values is greater than the sum of all 
+keys, the function should return the string "values".
+If keys and values have equal sums, the function should return 
+the string "balanced".
 
 def keys_v_values(dictionary):
     pass
@@ -19,3 +26,39 @@ Example Output:
 
 values
 keys'''
+
+def keys_v_values(dictionary):
+    keys =0
+    vals =0
+    #sum of all keys through .keys()
+    for i in dictionary.keys():
+        keys += i
+    #sum of all vals through.values()
+    for i in dictionary.values():
+        vals += i
+    
+    if keys>vals:
+        return "Keys"
+    elif vals>keys:
+        return "Values"
+    else:
+        return "balanced"
+# def keys_v_values(dictionary):
+#     keys=0
+#     vals=0
+#     for i in dictionary:
+#         keys +=i
+#     values = dictionary.values()
+#     vals = sum(values)
+#     if keys > vals:
+#         return "Keys"
+#     else:
+#         return "Values"
+    
+dictionary1 = {1:10, 2:20, 3:30, 4:40, 5:50, 6:60}
+greater_sum = keys_v_values(dictionary1)
+print(greater_sum)
+
+dictionary2 = {100:10, 200:20, 300:30, 400:40, 500:50, 600:60}
+greater_sum = keys_v_values(dictionary2)
+print(greater_sum)
