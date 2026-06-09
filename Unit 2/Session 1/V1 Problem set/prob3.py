419abbc6bba8bcf026fa8f1c4b21270ff1972a8c
Jana Pulak <jpulak@gmail.com>
2026-06-08 23:28:41 -0500

    s1-3

--- Unit 2/Session 1/V1 Problem set/prob3.py
+++ Unit 2/Session 1/V1 Problem set/prob3.py
@@ -1,5 +1,11 @@
 #Problem 3: Print Pair
-'''Write a function print_pair() that takes in a dictionary dictionary and a key target as parameters. The function looks for the target and when found, it prints the key and it's associated value as "Key: <key>" followed by "Value: <value>". If target is not in dictionary, print "That pair does not exist!".
+'''Write a function print_pair() that takes in a
+ dictionary dictionary and a key target as parameters.
+   The function looks for the target and when found, it
+     prints the key and it's associated value as 
+     "Key: <key>" followed by "Value: <value>". 
+     If target is not in dictionary, print 
+     "That pair does not exist!".
 
 def print_pair(dictionary, target):
     pass
@@ -16,3 +22,16 @@ Value: star
 That pair does not exist!
 Key: spongebob
 Value: squarepants'''
+
+def print_pair(dictionary, target):
+    # if dictionary.get(target):
+    if target in dictionary:
+        print("Key: "+ target)
+        print("Value: " + dictionary[target])
+    else:
+        print("That pair does not exist!")
+    
+dictionary = {"spongebob": "squarepants", "patrick": "star", "squidward": "tentacles"}
+print_pair(dictionary, "patrick")
+print_pair(dictionary, "plankton")
+print_pair(dictionary, "spongebob")
