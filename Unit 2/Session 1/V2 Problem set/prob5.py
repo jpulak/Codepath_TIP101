2743439f6d6dc47bf0de935a54ea68579ecb45eb
Jana Pulak <jpulak@gmail.com>
2026-06-09 01:29:06 -0500

    s2-5

--- Unit 2/Session 1/V2 Problem set/prob5.py
+++ Unit 2/Session 1/V2 Problem set/prob5.py
@@ -1,5 +1,11 @@
 #Problem 5: Merge Catalogs
-'''Write a function merge_catalogs() that combines two product catalogs, catalog1 and catalog2 as parameters. Each parameter is a dictionary where each key-value pair represents a product name and its price, respectively. If the same product exists in both catalogs, the price from the second catalog should overwrite the price in the first. Return the updated first catalog dictionary.
+'''Write a function merge_catalogs() that combines two 
+product catalogs, catalog1 and catalog2 as parameters. 
+Each parameter is a dictionary where each key-value pair
+ represents a product name and its price, respectively. 
+ If the same product exists in both catalogs, the price
+   from the second catalog should overwrite the price in 
+   the first. Return the updated first catalog dictionary.
 
 def merge_catalogs(catalog1, catalog2):
     pass
@@ -8,3 +14,13 @@ Example Input:
 catalog1 = {"apple": 1.0, "banana": 0.5}
 catalog2 = {"banana": 0.75, "cherry": 1.25}
 Example Output: {'apple': 1.0, 'banana': 0.75, 'cherry': 1.25}'''
+
+def merge_catalogs(catalog1, catalog2):
+    for item, val in catalog2.items():
+        catalog1[item] = val
+    return catalog1
+
+catalog1 = {"apple": 1.0, "banana": 0.5}
+catalog2 = {"banana": 0.75, "cherry": 1.25}
+
+print(merge_catalogs(catalog1, catalog2))
