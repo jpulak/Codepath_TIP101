7bf6353129935661be0898f402d056cc569d9417
Jana Pulak <jpulak@gmail.com>
2026-06-09 16:23:37 -0500

    s3-2

--- Unit 2/Session 1/V3 Problem set/prob2.py
+++ Unit 2/Session 1/V3 Problem set/prob2.py
@@ -1,5 +1,11 @@
 #Problem 2: Build Inventory
-'''Write a function build_inventory() that takes in a list of product_names and a corresponding list of product_prices as parameters. The function returns a dictionary representing the inventory of a small store. Each product name in product_names should be a key in the dictionary and the corresponding value should be the item price.
+'''Write a function build_inventory() that takes in a 
+list of product_names and a corresponding list of 
+product_prices as parameters. The function returns a 
+dictionary representing the inventory of a small store. 
+Each product name in product_names should be a key in the 
+dictionary and the corresponding value should be the item 
+price.
 
 product_names[i] should be paired with product_prices[i] in the dictionary where 0 <= i <= len(product_names). You may assume that product_names and product_prices are the same length.
 
@@ -13,3 +19,20 @@ print(build_inventory(product_names, product_prices))
 Example Output:
 
 {'Apple': 0.99, 'Banana': 0.5, 'Orange': 0.75}'''
+
+def build_inventory(product_names, product_prices):
+
+    invent = {}
+    for i in range(len(product_names)):
+        names = product_names[i]
+        price = product_prices[i]
+        invent[names]= price
+    return invent
+    # invent ={}
+    # for i in range (len(product_names)):
+    #     invent[product_names[i]]= product_prices[i]
+    # return invent
+
+product_names = ["Apple", "Banana", "Orange"]
+product_prices = [0.99, 0.50, 0.75]
+print(build_inventory(product_names, product_prices))
