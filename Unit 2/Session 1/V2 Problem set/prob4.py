b11a1d68ec35cdd31fb1dce6536a62b68ea7fd8e
Jana Pulak <jpulak@gmail.com>
2026-06-09 01:19:29 -0500

    s2-4

--- Unit 2/Session 1/V2 Problem set/prob4.py
+++ Unit 2/Session 1/V2 Problem set/prob4.py
@@ -1,5 +1,7 @@
 #Problem 4: Sum Even Values
-'''Write a function sum_even_values() that returns the sum of all even values in a given dictionary. Assume the dictionary values are all integers.
+'''Write a function sum_even_values() that returns
+ the sum of all even values in a given dictionary.
+   Assume the dictionary values are all integers.
 
 def sum_even_values(dictionary):
     pass
@@ -8,3 +10,22 @@ Example Usage:
 dictionary = {"a": 4, "b": 1, "c": 2, "d": 8, }
 print(sum_even_values(dictionary))
 Example Output: 14'''
+
+def sum_even_values(dictionary):
+    total =0
+    for i in dictionary.values():
+        if i%2 ==0:
+            total += i
+    return total
+
+dictionary = {"a": 4, "b": 1, "c": 2, "d": 8, }
+print(sum_even_values(dictionary))
+
+#bracket notation
+# def sum_even_values(dictionary):
+#     sum_even = 0
+#     for key in dictionary:
+#         if dictionary[key] % 2 == 0:
+#             sum_even += dictionary[key]
+#     return sum_even
+
