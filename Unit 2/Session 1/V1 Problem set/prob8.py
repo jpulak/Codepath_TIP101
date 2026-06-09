fc315706f33f75aa34acd0a26b1e8a56f9326b50
Jana Pulak <jpulak@gmail.com>
2026-06-09 00:52:37 -0500

    s1-8

--- Unit 2/Session 1/V1 Problem set/prob8.py
+++ Unit 2/Session 1/V1 Problem set/prob8.py
@@ -1,8 +1,19 @@
 #Problem 8: Index-Value Map
-'''Write a function index_to_value_map() that takes in a list lst and returns a dictionary that maps the index of each element in lst to its value.
+'''Write a function index_to_value_map() that takes
+ in a list lst and returns a dictionary that maps the
+   index of each element in lst to its value.
 
 def index_to_value_map(lst):
     pass
 Example Input: lst = ["apple", "banana", "cherry"]
 
 Example Output: {0: "apple", 1: "banana", 2: "cherry"}'''
+
+def index_to_value_map(lst):
+    dictt ={}
+    for i in range(len(lst)):
+        dictt[i] = lst[i]
+    return dictt
+
+lst = ["apple", "banana", "cherry"]
+print(index_to_value_map(lst))
