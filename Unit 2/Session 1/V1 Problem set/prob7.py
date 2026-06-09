f47896198817dab3b5d1097e2c391655806982f9
Jana Pulak <jpulak@gmail.com>
2026-06-09 00:49:48 -0500

    s1-7

--- Unit 2/Session 1/V1 Problem set/prob7.py
+++ Unit 2/Session 1/V1 Problem set/prob7.py
@@ -1,7 +1,13 @@
 #Problem 7: Best Book
-'''Imagine you are working on a book review software like Goodreads. Write a function named highest_rated() that returns the book with the highest rating.
+'''Imagine you are working on a book review software like
+ Goodreads. Write a function named highest_rated() that 
+ returns the book with the highest rating.
 
-The function should take in a list of dictionaries named books as a parameter. Each dictionary represents data associated with a book, including its title, author, and rating. The function should return the dictionary for the book with the highest rating.
+The function should take in a list of dictionaries named 
+books as a parameter. Each dictionary represents data 
+associated with a book, including its title, author, and 
+rating. The function should return the dictionary for the 
+book with the highest rating.
 
 def highest_rated(books):
     pass
@@ -27,3 +33,31 @@ Expected Output:
  "author": "Hanif Abdurraqib",
  "rating": 4.47
 }'''
+
+def highest_rated(books):
+
+    if not books: # if empty
+        return None
+    
+    high = books[0]
+    for book in books[1:]: #start from second book
+        if book["rating"] > high["rating"]:
+            high = book
+    return high
+
+books = [
+    {"title": "Tomorrow, and Tomorrow, and Tomorrow",
+     "author": "Gabrielle Zevin",
+     "rating": 4.18
+    },
+    {"title": "A Fortune For Your Disaster",
+     "author": "Hanif Abdurraqib",
+     "rating": 4.47
+    },
+    {"title": "The Seven Husbands of Evenlyn Hugo",
+     "author": "Taylor Jenkins Reid",
+     "rating": 4.40
+    }
+]
+
+print(highest_rated(books))
