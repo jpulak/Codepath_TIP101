5792d010ef14fd86f7a434f64b59f102415e0396
Jana Pulak <jpulak@gmail.com>
2026-06-09 01:16:53 -0500

    s2-3

--- Unit 2/Session 1/V2 Problem set/prob3.py
+++ Unit 2/Session 1/V2 Problem set/prob3.py
@@ -1,11 +1,15 @@
 #Problem 3: Dictionary Description
 
 '''The following function get_description() takes in a 
-dictionary info and a list keys as parameters. For each key in keys, 
-the function prints the value associated with that key in info and prints None if the key does not exist in info.
+dictionary info and a list keys as parameters. 
+For each key in keys, 
+the function prints the value associated with that 
+key in info and prints None if the key does not exist in info.
 
-However, the function has a bug! Copy the function into your IDE 
-and run the code. Work with your group members to find the cause of the
+However, the function has a bug! Copy the function 
+into your IDE 
+and run the code. Work with your group members to 
+find the cause of the
  bug and correctly implement the function.
 
 def get_description(info, keys):
@@ -21,4 +25,19 @@ Expected Output:
 Tom
 engineer
 None
-Note: The actual input you see when you run the provided code without fixing the bug may be different!'''
+Note: The actual input you see when you run the provided
+ code without fixing the bug may be different!'''
+
+def get_description(info, keys):
+
+    for key in keys:
+        if key in info:
+            print(info[key])
+        else:
+            print(None)
+    # for i in info.values():
+    #     print(i)
+          
+info = {"name": "Tom", "age": "30", "occupation": "engineer"}
+keys = ["name", "occupation", "salary"]
+get_description(info, keys)
