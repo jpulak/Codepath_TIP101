b743a84a6bf363ce6fc3c6b494f7ed4842fd68e6
Jana Pulak <jpulak@gmail.com>
2026-06-09 00:31:33 -0500

    s1-

--- Unit 2/Session 1/V1 Problem set/prob5.py
+++ Unit 2/Session 1/V1 Problem set/prob5.py
@@ -1,9 +1,16 @@
 #Problem 5: Restock Inventory
-'''Write a function restock_inventory() that updates an inventory dictionary based on a restock list. It accepts two parameters:
+'''Write a function restock_inventory() that updates an
+ inventory dictionary based on a restock list. It accepts 
+ two parameters:
 
-current_inventory: a dictionary where each key-value pair represents an item and its current stock in the inventory
-restock_list: a dictionary where each key-value pair represents an item and the quantity to be added to the inventory
-If an item in restock_list is not present in the current_inventory, it should be added. The function should return the updated dictionary current_inventory.
+current_inventory: a dictionary where each key-value pair 
+represents an item and its current stock in the inventory
+restock_list: a dictionary where each key-value pair 
+represents an item and the quantity to be added to the 
+inventory
+If an item in restock_list is not present in the
+ current_inventory, it should be added. The function 
+ should return the updated dictionary current_inventory.
 
 def restock_inventory(current_inventory, restock_list):
     pass
@@ -28,3 +35,37 @@ Example Output:
     "oranges": 30,
     "pears": 5
 }'''
+#provided solution 
+# def restock_inventory(current_inventory, restock_list):
+
+#     # Loop over restock_list items to update current_inventory
+#     for item, quantity in restock_list.items():
+#         if item in current_inventory:
+#             current_inventory[item] += quantity
+#         else:
+#             current_inventory[item] = quantity
+    
+#     return current_inventory
+
+def restock_inventory(current_inventory, restock_list):
+    for k,v in restock_list.items():
+        if k in current_inventory:
+            current_inventory[k]+=v
+        else:
+            current_inventory[k] = v
+    return current_inventory 
+
+
+current_inventory = {
+    "apples": 30,
+    "bananas": 15,
+    "oranges": 10
+}
+
+restock_list = {
+    "oranges": 20,
+    "apples": 10,
+    "pears": 5
+}
+
+print(restock_inventory(current_inventory, restock_list))
