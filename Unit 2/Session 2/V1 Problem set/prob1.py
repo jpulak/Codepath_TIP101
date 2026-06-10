c2bbc7cefdda2d871d50aaa7c5fea9b9a15600a8
Jana Pulak <jpulak@gmail.com>
2026-06-10 12:11:32 -0500

    s1-1

--- Unit 2/Session 2/V1 Problem set/prob1.py
+++ Unit 2/Session 2/V1 Problem set/prob1.py
@@ -1,5 +1,11 @@
 #Problem 1: Cast Vote
-'''Write a function cast_vote() that records a vote for a candidate in an election. The function accepts a dictionary votes that maps candidates to their current number of votes and a string candidate that represents the candidate the user would like to vote for. If the candidate doesn't exist, add them to the dictionary. The function should return the updated dictionary.
+'''Write a function cast_vote() that records a vote 
+for a candidate in an election. The function accepts a
+ dictionary votes that maps candidates to their current 
+ number of votes and a string candidate that represents the 
+ candidate the user would like to vote for. If the candidate
+   doesn't exist, add them to the dictionary. The function 
+   should return the updated dictionary.
 
 def cast_vote(votes, candidate):
     pass
@@ -14,3 +20,22 @@ Example Output:
 
 {'Alice': 6, 'Bob': 3}
 {'Alice': 6, 'Bob': 3, 'Gina': 1}'''
+
+def cast_vote(votes, candidate):
+    if candidate in votes:
+        votes[candidate] +=1
+    else:
+        votes[candidate] = 1
+
+# def cast_vote(votes, candidate):
+#     if candidate not in votes:
+#         votes[candidate] = 0
+#     votes[candidate] += 1
+#     return votes
+
+
+votes = {"Alice": 5, "Bob": 3}
+cast_vote(votes, "Alice")
+print(votes)
+cast_vote(votes, "Gina")
+print(votes)
