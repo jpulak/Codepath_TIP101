d1d1df3dd6068f934689d84267bb588006c45a06
Jana Pulak <jpulak@gmail.com>
2026-06-10 20:58:04 -0500

    s1-2

--- Unit 2/Session 2/V1 Problem set/prob2.py
+++ Unit 2/Session 2/V1 Problem set/prob2.py
@@ -1,5 +1,7 @@
 #Problem 2: Keys in Common
-'''Write a function that takes in two dictionaries, dict1 and dict2 and finds all keys common to both dictionaries. The function returns a list of common keys.
+'''Write a function that takes in two dictionaries,
+ dict1 and dict2 and finds all keys common to both 
+ dictionaries. The function returns a list of common keys.
 
 def common_keys(dict1, dict2):
 	pass
@@ -12,3 +14,24 @@ print(common_list)
 Example Output:
 
 ['b', 'c']'''
+
+#worst time complexity
+def common_keys(dict1, dict2):
+	all=[]
+	for i in dict1:
+		for k in dict2:
+			if i == k:
+				all.append(k)
+	return all
+
+dict1 = {"a": 1, "b": 2, "c": 3}
+dict2 = {"b": 4, "c": 5, "d": 6}
+common_list = common_keys(dict1, dict2)
+print(common_list)
+
+# def common_keys(dict1, dict2):
+#     common = []
+#     for key in dict1:
+#         if key in dict2:
+#             common.append(key)
+#     return common
