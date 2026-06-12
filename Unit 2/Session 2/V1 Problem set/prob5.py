6594b4221d71e50bd9f70788b58e1a58ad4c96b0
Jana Pulak <jpulak@gmail.com>
2026-06-12 01:11:07 -0500

    s1-5

--- Unit 2/Session 2/V1 Problem set/prob5.py
+++ Unit 2/Session 2/V1 Problem set/prob5.py
@@ -1,5 +1,9 @@
 #Problem 5: Find Majority Element
-'''Write a function find_majority_element() that takes in a list of integers elements and finds the majority element in the list. A majority element is an element that appears more than n/2 times where n is the size of the list. If there is no majority element, return None.
+'''Write a function find_majority_element() that takes in 
+a list of integers elements and finds the majority element 
+in the list. A majority element is an element that appears more
+ than n/2 times where n is the size of the list. If there is no
+   majority element, return None.
 
 def find_majority_element(elements):
     pass
@@ -8,3 +12,20 @@ Example Usage:
 elements = [2, 2, 1, 1, 1, 2, 2]
 print(find_majority_element(elements))
 Example Output: 2'''
+
+def find_majority_element(elements):
+    new ={}
+    for num in elements:
+        if num not in new:
+            new[num] = 0
+        new[num] +=1
+
+    for key,val in new.items():
+        if val > len(elements)/2:
+            return key
+        # else:
+        #     return None
+    return None
+
+elements = [2, 2, 1, 1, 1, 2, 2]
+print(find_majority_element(elements))
