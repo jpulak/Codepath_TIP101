80a0d63d24d3a8a115ee8aa8a7f5fb7900a2dd07
Jana Pulak <jpulak@gmail.com>
2026-06-11 22:53:26 -0500

    s1-4

--- Unit 2/Session 2/V1 Problem set/prob4.py
+++ Unit 2/Session 2/V1 Problem set/prob4.py
@@ -1,12 +1,18 @@
 #Problem 4: Highest Priority Task
-'''Given a dictionary tasks where keys are task names and values are priorities (1-10, where 10 is the highest priority), write a function get_highest_priority_task() that removes the task with the highest priority from the dictionary and returns its name.
-If two tasks have the same priority, return the task that comes first in the alphabet.
+'''Given a dictionary tasks where keys are task names 
+and values are priorities (1-10, where 10 is the highest
+ priority), write a function get_highest_priority_task()
+   that removes the task with the highest priority from the 
+   dictionary and returns its name.
+If two tasks have the same priority, return the task that 
+comes first in the alphabet.
 
 def get_highest_priority_task(tasks):
 	pass
 Example Usage:
 
-tasks = {"task1": 8, "task2": 10, "task3": 9, "task4": 10, "task5": 7}
+tasks = {"task1": 8, "task2": 10, "task3": 9, "task4": 10, 
+"task5": 7}
 perform_task = (get_highest_priority_task(tasks))
 print(perform_task)
 
@@ -23,3 +29,21 @@ task2
 task4
 task3
 {'task1': 8,'task5': 7}'''
+
+def get_highest_priority_task(tasks):
+	if not tasks:
+		return None
+	
+	
+
+tasks = {"task1": 8, "task2": 10, "task3": 9, "task4": 10, "task5": 7}
+perform_task = (get_highest_priority_task(tasks))
+print(perform_task)
+
+perform_task = (get_highest_priority_task(tasks))
+print(perform_task)
+
+perform_task = (get_highest_priority_task(tasks))
+print(perform_task)
+
+print(tasks)
