a251ba519733d3832c8b137ca1c4ccc6260f7406
Jana Pulak <jpulak@gmail.com>
2026-06-12 01:33:01 -0500

    s1-6

--- Unit 2/Session 2/V1 Problem set/prob6.py
+++ Unit 2/Session 2/V1 Problem set/prob6.py
@@ -25,4 +25,20 @@ True
 True'''
 
 def has_duplicates(nums, k):
-	pass
+	recent_index ={}
+	for i in range (len(nums)):
+		if nums[i] in recent_index:
+			diff = i - recent_index[nums[i]]
+			if diff <=k:
+				return True
+			
+		recent_index[nums[i]] =i
+	return False
+
+nums = [5, 6, 8, 2, 6, 4, 9]
+check1 = has_duplicates(nums, 2)
+print(check1)
+check2 = has_duplicates(nums, 5)
+print(check2)
+check3 = has_duplicates(nums, 3)
+print(check3)
