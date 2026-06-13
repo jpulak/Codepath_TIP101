a90a74e0b3626dcd164d6214c0a005f742836531
Jana Pulak <jpulak@gmail.com>
2026-06-13 01:50:09 -0500

    s2-2

--- Unit 2/Session 2/V2 Problem set/prob2.py
+++ Unit 2/Session 2/V2 Problem set/prob2.py
@@ -1,5 +1,7 @@
 #Problem 2: Dictionary Intersection
-'''Write a function dict_intersection() that takes in two dictionaries as parameters and returns a new dictionary containing the key-value pairs found in both dictionaries.
+'''Write a function dict_intersection() that takes in two 
+dictionaries as parameters and returns a new dictionary 
+containing the key-value pairs found in both dictionaries.
 
 def dict_intersection(d1, d2):
     pass
@@ -8,3 +10,18 @@ Example Input:
 d1 = {'a': 1, 'b': 2, 'c': 3}
 d2 = {'b': 2, 'c': 4}
 Example Output: {'b': 2}'''
+
+def dict_intersection(d1, d2):
+    both={}
+    for key in d1:
+        if key in d2 and d2[key] == d1[key]:
+                both[key] = d1[key]
+
+    return both
+            
+
+
+d1 = {'a': 1, 'b': 2, 'c': 3}
+d2 = {'b': 2, 'c': 4}
+
+print(dict_intersection(d1, d2))
