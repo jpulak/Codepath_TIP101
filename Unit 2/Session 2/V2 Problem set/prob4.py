bb0253edfeae88a5301c615b42ff265983b625ba
Jana Pulak <jpulak@gmail.com>
2026-06-13 02:07:17 -0500

    s2-4

--- Unit 2/Session 2/V2 Problem set/prob4.py
+++ Unit 2/Session 2/V2 Problem set/prob4.py
@@ -17,3 +17,37 @@ Example Output:
 
 [1,3]'''
 
+'''
+u
+i understand
+function, parameters (list of integer) list has even number of frequency
+except two, two nums occur odd frequencies, return odds into a list
+
+define function with paramter of list
+create new dictionary
+putt numbers form list into dictionary(iterate)
+
+initialize new list
+iterate through dictionary values to see what is odd
+for every odd value, append into new list
+
+edge case, nothing in list, none found(however stated always will be)
+
+'''
+def find_odd_occurrences(numbers):
+    new={}
+    for num in numbers:
+        if num not in new:
+            new[num] =0
+        new[num] +=1
+
+    ans=[]
+    for key,val in new.items():
+        if val %2!=0:
+            ans.append(key)
+
+    return ans 
+
+numbers = [1,4,2,3,2,3,3,4,4,4]
+odd_list = find_odd_occurrences(numbers)
+print(odd_list)
