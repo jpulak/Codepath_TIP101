aac7f062858b241f13ed848d269284fde6644d9f
Jana Pulak <jpulak@gmail.com>
2026-06-13 00:55:26 -0500

    s2-1

--- Unit 2/Session 2/V2 Problem set/prob1.py
+++ Unit 2/Session 2/V2 Problem set/prob1.py
@@ -1,5 +1,9 @@
 #Problem 1: Update Score
-'''Write a function update_score() that takes in a dictionary scores that maps player names to current scores, a string player, and an integer points as parameters. The function adds the points to the current score of player in the dictionary and returns the updated dictionary. If the player does not exist in scores, then add them.
+'''Write a function update_score() that takes in a dictionary
+ scores that maps player names to current scores, a string player, and 
+ an integer points as parameters. The function adds the points to the
+   current score of player in the dictionary and returns the updated 
+   dictionary. If the player does not exist in scores, then add them.
 
 def update_score(scores, players, points):
     pass
@@ -17,3 +21,19 @@ Example Output:
 {'Alice': 110, 'Bob': 90}
 {'Alice': 110, 'Bob': 90, 'Calvin': 20}
 {'Alice': 110, 'Bob': 90, 'Calvin': 25}'''
+
+def update_score(scores, players, points):
+        if players not in scores:
+            scores[players] = points
+        else:
+              scores[players] += points
+        return scores
+    
+    
+scores = {"Alice": 100, "Bob": 90}
+update_score(scores, "Alice", 10)
+print(scores)
+update_score(scores, "Calvin", 20)
+print(scores)
+update_score(scores, "Calvin", 5)
+print(scores)
