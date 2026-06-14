6951b4d8cc053268b521a92322d4576220e4c272
Jana Pulak <jpulak@gmail.com>
2026-06-13 20:24:48 -0500

    s2-7

--- Unit 2/Session 2/V2 Problem set/prob7.py
+++ Unit 2/Session 2/V2 Problem set/prob7.py
@@ -1,5 +1,6 @@
 #Problem 7: Good Pairs
-'''Write a function num_identical_pairs() that takes in a list of integers nums and returns the number of good pairs.
+'''Write a function num_identical_pairs() that takes in a list of 
+integers nums and returns the number of good pairs.
 A pair (i, j) is called good if nums[i] == nums[j] and i < j.
 
 def num_identical_pairs(nums):
@@ -46,3 +47,22 @@ nums[5] == 3
 *will not check any any indices less than current index*
 No identical pairs found
 '''
+
+def num_identical_pairs(nums):
+    freq={}
+    for num in nums:
+        freq[num] = freq.get(num, 0) + 1
+    good_pairs = 0
+    for count in freq.values():
+        good_pairs += count * (count - 1) // 2  # Combination of 2 from n
+        
+    return good_pairs
+
+nums = [1,2,3,1,1,3]
+print(num_identical_pairs(nums))
+
+nums = [1,2,3]
+print(num_identical_pairs(nums))
+
+nums = [1,1,1,1]
+print(num_identical_pairs(nums))
