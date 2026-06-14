eb7e71d35dca5cbe4a241f02026db7adb5604ce2
Jana Pulak <jpulak@gmail.com>
2026-06-13 20:16:12 -0500

    s2-6

--- Unit 2/Session 2/V2 Problem set/prob6.py
+++ Unit 2/Session 2/V2 Problem set/prob6.py
@@ -1,5 +1,8 @@
 #Problem 6: How Many Smaller
-'''Write a function smaller_numbers_than_current() that takes in a list of numbers nums as a parameter. For each nums[i], the function should find out how many numbers in the list are smaller than it. (For each nums[i], count the number of valid j's such that j!=i and nums[j] < nums[i])
+'''Write a function smaller_numbers_than_current() that takes in a list of 
+numbers nums as a parameter. For each nums[i], the function should find out 
+how many numbers in the list are smaller than it. (For each nums[i], count the
+ number of valid j's such that j!=i and nums[j] < nums[i])
 
 Return the answers in a list.
 
@@ -28,3 +31,55 @@ There exists one smaller number than it (1) --> ans[3]=1
 nums[4] == 3 
 There exists three smaller numbers than it (1, 2 and 2) --> ans[4]=3
 '''
+'''
+understand
+function taking in a list of integers
+for each integer, figure out how many numbers within the list is smaller than the integer
+output the number into a list
+
+plan
+define function with parameter(list of integer)
+define new list
+define new var to keep track??
+iterate through the list
+current = i
+    for i in range
+    if i < i:
+        var +=1
+nested list?
+
+implement
+
+def smaller_numbers_than_current(nums):
+    all=[]
+    for num in nums:
+        new = num
+        track =0
+        for j in nums:
+            if new > j:
+                track +=1
+        all.append(track)
+    return all
+'''
+
+def smaller_numbersT_than_current(nums):
+    # Sort the nums list
+    sorted_nums = sorted(nums)
+    
+    # Dictionary to store the smallest index at which each number appears
+    smaller_count_map = {}
+    for i in range(len(sorted_nums)):
+        num = sorted_nums[i]
+        # Only set the index for the first occurrence of each number
+        if num not in smaller_count_map:
+            smaller_count_map[num] = i
+    
+    # Manually generate the result based on the original list values
+    result = []
+    for num in nums:
+        result.append(smaller_count_map[num])
+    
+    return result
+
+nums = [6,1,2,2,3]
+print(smaller_numbers_than_current(nums))
