3915a1d7a929bba8c3e65d55c1ddba67ff256f91
Jana Pulak <jpulak@gmail.com>
2026-06-14 17:24:12 -0500

    s3-2

--- Unit 2/Session 2/V3 Problem set/prob2.py
+++ Unit 2/Session 2/V3 Problem set/prob2.py
@@ -1,5 +1,8 @@
 #Problem 2: Dictionary Difference
-'''Write a function dict_difference() that takes two dictionaries and returns a new dictionary that contains only the key-value pairs found only in the first dictionary but not in the second.
+'''Write a function dict_difference() that takes two 
+dictionaries and returns a new dictionary that contains 
+only the key-value pairs found only in the first dictionary 
+but not in the second.
 
 def dict_difference(d1, d2):
     pass
@@ -9,3 +12,35 @@ d1 = {'a': 1, 'b': 2, 'c': 3, 'd': 4}
 d2 = {'b': 2, 'd': 1}
 print(dict_difference(d2, d1))
 Example Output: {'a': 1, 'c': 3, 'd': 4}'''
+
+
+'''
+understand
+
+i understand that 
+i need to make a function that takes in two different dictionaries
+return a new dictionary  that only has the key-value pair that is found
+ONLY in th efirst dict not in hte second
+
+plan
+
+declare function (d1,d2):
+initialize new dictionary
+iterate through BOTH key and val in d1
+if key and val of d1 not in d2
+add that key and val into new dictionary
+return new dictionary
+
+implement
+
+'''
+def dict_difference(d1, d2):
+    new={}
+    for key in d1:
+        if key not in d2 or d1[key] != d2[key]: 
+            new[key] = d1[key]
+    return new
+
+d1 = {'a': 1, 'b': 2, 'c': 3, 'd': 4}
+d2 = {'b': 2, 'd': 1}
+print(dict_difference(d2, d1))
