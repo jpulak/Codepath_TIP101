ce8853d8b187ceed579db3621c1f59fdbf68c182
Jana Pulak <jpulak@gmail.com>
2026-06-15 00:51:51 -0500

    s3-4

--- Unit 2/Session 2/V3 Problem set/prob4.py
+++ Unit 2/Session 2/V3 Problem set/prob4.py
@@ -1,5 +1,8 @@
 #Problem 4: Group By Frequency
-'''Write a function group_by_frequency() that takes in a list lst and returns a dictionary where keys represent the frequency of unique elements within lst and values represent a list of elements with the same frequency.
+'''Write a function group_by_frequency() that takes in a list
+ lst and returns a dictionary where keys represent the frequency
+   of unique elements within lst and values represent a list of 
+   elements with the same frequency.
 
 def group_by_frequency(lst):
     pass
@@ -10,3 +13,21 @@ print(group_by_frequency(lst))
 Example Output:
 
 { 1 : ['a', 'b'], 2: ['c', 'd'], 3: ['e']}'''
+
+
+def group_by_frequency(lst):
+    new={}
+    for i in lst:
+        new[i]=new.get(i,0)+1
+    
+    rev={}
+    for letter,num in new.items():
+        if num not in rev:
+            rev[num]=[]
+        rev[num].append(letter)
+
+    return rev
+
+
+lst = ['a', 'b', 'c', 'd', 'd', 'c', 'e', 'e', 'e']
+print(group_by_frequency(lst))
