25f5f50839da63c74a92420fa535021e3eea1324
Jana Pulak <jpulak@gmail.com>
2026-06-15 00:57:20 -0500

    s3-5

--- Unit 2/Session 2/V3 Problem set/prob5.py
+++ Unit 2/Session 2/V3 Problem set/prob5.py
@@ -1,5 +1,8 @@
 #Problem 5: No Duplicates Allowed
-'''Write a function that takes in a list of integers nums as a parameter and removes all duplicates. The function should return a list containing the unique elements in the order of their last occurrence in the original list.
+'''Write a function that takes in a list of integers nums 
+as a parameter and removes all duplicates. The function should
+ return a list containing the unique elements in the order of 
+ their last occurrence in the original list.
 
 def remove_duplicates_from_front(nums):
     pass
@@ -8,3 +11,25 @@ Example Input:
 nums = [6,5,3,5,2,8,3]
 print(remove_duplicates_from_front(nums))
 Example Output: [6,5,2,8,3]'''
+
+def remove_duplicates_from_front(nums):
+    unique_nums = []
+    for i in range(len(nums)-1, -1, -1):
+        if nums[i] not in unique_nums:
+            unique_nums.insert(0, nums[i])
+    return unique_nums
+
+
+nums = [6,5,3,5,2,8,3]
+print(remove_duplicates_from_front(nums))
+
+# def remove_duplicates_from_front(nums):
+#     frequency_map = {}
+#     for num in nums:
+#         frequency_map[num] = True
+#     last_occurrences = []
+#     for num in nums[::-1]:  # Reverse iterate
+#         if frequency_map[num]:
+#             last_occurrences.append(num)
+#             frequency_map[num] = False
+#     return last_occurrences[::-1]  # Reverse again to original order
