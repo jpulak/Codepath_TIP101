33bee63b64decf01b6a0c40f06274bd95bcc43fa
Jana Pulak <jpulak@gmail.com>
2026-06-15 22:44:55 -0500

    s1-1

--- Unit 3/Session 1/V1 Problem set/prob1.py
+++ Unit 3/Session 1/V1 Problem set/prob1.py
@@ -4,10 +4,19 @@
 def count_mississippi(limit):
     for num in range(1, limit):
         print( f"{num} mississippi")
-Call the function so that it prints out the following to the console (without calling the function more than once):
+Call the function so that it prints out the following to the console 
+(without calling the function more than once):
 
 1 mississipi
 2 mississipi
 3 mississipi
 4 mississipi
 5 mississipi'''
+
+def count_mississippi(limit):
+    for num in range(1, limit):
+        print( f"{num} mississippi")
+
+
+limit= 6
+count_mississippi(limit)
