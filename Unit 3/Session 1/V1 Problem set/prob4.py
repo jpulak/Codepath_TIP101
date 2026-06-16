f9d7968ce3653e7fbfdbb583f63daa9be8ddb1c2
Jana Pulak <jpulak@gmail.com>
2026-06-16 17:25:49 -0500

    s1-4

--- Unit 3/Session 1/V1 Problem set/prob4.py
+++ Unit 3/Session 1/V1 Problem set/prob4.py
@@ -1,5 +1,7 @@
 #Problem 4: Reverse String
-'''Write a function reverse_string() that takes a string my_str as a parameter and returns the string reversed.
+'''Write a function reverse_string() that 
+takes a string my_str as a parameter and 
+returns the string reversed.
 
 def reverse_string(my_str):
     pass
@@ -8,3 +10,16 @@ Example Usage:
 my_str = "live"
 print(reverse_string(my_str))
 Example Output: evil'''
+
+
+def reverse_string(my_str):
+    return my_str[::-1]
+
+my_str = "live"
+print(reverse_string(my_str))
+
+# def reverse_string(my_str):
+# 	reversed_str = ''
+# 	for char in my_str:
+# 		reversed_str = char + reversed_str
+# 	return reversed_str
