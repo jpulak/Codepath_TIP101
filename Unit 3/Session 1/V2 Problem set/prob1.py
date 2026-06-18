f50f34f7910520b674a7de843d7bc7a763ed42a7
Jana Pulak <jpulak@gmail.com>
2026-06-17 23:57:56 -0500

    s2-1

--- Unit 3/Session 1/V2 Problem set/prob1.py
+++ Unit 3/Session 1/V2 Problem set/prob1.py
@@ -4,9 +4,14 @@
 def match_made(dictionary):
     for key, value in dictionary.items():
         print( f"{key} and {value} are a perfect match.")
-Add code to your IDE so that your program prints out the following to the console:
+Add code to your IDE so that your program prints out the 
+following to the console:
 
 Peanut butter and Jelly are a perfect match.
 Spongebob and Patrick are a perfect match.
 Ash and Pikachu are a perfect match.
 '''
+
+def match_made(dictionary):
+    for key, value in dictionary.items():
+        print( f"{key} and {value} are a perfect match.")
