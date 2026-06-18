d8a07c203b221138677f57d75400347595b1682e
Jana Pulak <jpulak@gmail.com>
2026-06-18 17:40:08 -0500

    s2-4

--- Unit 3/Session 1/V2 Problem set/prob4.py
+++ Unit 3/Session 1/V2 Problem set/prob4.py
@@ -1,5 +1,11 @@
 #Problem 4: Reverse Sentence
-'''Write a function reverse_sentence() that takes in a string sentence as a parameter and returns the string with the sentence but with the order of the words reversed. The sentence will only contain alphabetic characters and spaces to separate the words. If there is only one word in the sentence, the function returns the original string.
+'''Write a function reverse_sentence() that takes in 
+ string sentence as a parameter and returns the string
+   with the sentence but with the order of the words
+     reversed. The sentence will only contain alphabetic 
+     characters and spaces to separate the words. If 
+     there is only one word in the sentence, the function 
+     returns the original string.
 
 def reverse_sentence(sentence):
     pass
@@ -8,3 +14,17 @@ Example Input: sentence = "I solemnly swear I am up to no good"
 Example Output: "good no to up am I swear solemnly I"
 
 '''
+
+def reverse_sentence(sentence):
+    res=""
+    new = sentence.split()
+
+    #used no functions
+    for word in new:
+        res = word +" "+ res
+    return res
+
+    # return (" ".join(new[::-1]))
+
+sentence = "I solemnly swear I am up to no good"
+print(reverse_sentence(sentence))
