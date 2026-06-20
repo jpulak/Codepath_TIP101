f925733482124625d32b90b9a7d7dcdbbe1e7cfe
Jana Pulak <jpulak@gmail.com>
2026-06-20 00:43:09 -0500

    s1-6

--- Unit 3/Session 1/V2 Problem set/prob6.py
+++ Unit 3/Session 1/V2 Problem set/prob6.py
@@ -1,5 +1,8 @@
 #Problem 6: Needle in a Haystack
-'''Write a function find_the_needle() that takes in two string parameters: a needle and a haystack. The function returns the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+'''Write a function find_the_needle() that takes in two
+ string parameters: a needle and a haystack. The function 
+ returns the index of the first occurrence of needle in haystack, 
+ or -1 if needle is not part of haystack.
 
 def find_the_needle(haystack, needle):
     pass
@@ -18,3 +21,19 @@ Example Output:
 -1
 
 '''
+
+def find_the_needle(haystack, needle):
+
+    if not needle:
+        return 0
+    
+
+    return haystack.find(needle)
+
+haystack = "tobeornottobe"
+needle = "be"
+print(find_the_needle(haystack, needle))
+
+haystack2 = "leetcode"
+needle2 = "leeto"
+print(find_the_needle(haystack2, needle2))
