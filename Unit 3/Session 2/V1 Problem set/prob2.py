d472ae840107aa135588d5857411f4b3700e6eb1
Jana Pulak <jpulak@gmail.com>
2026-06-23 17:10:41 -0500

    s3-2

--- Unit 3/Session 2/V1 Problem set/prob2.py
+++ Unit 3/Session 2/V1 Problem set/prob2.py
@@ -1,7 +1,8 @@
 #Problem 2: Remove Duplicates
 '''Write a function remove_duplicates() that takes in 
 a sorted list of integers nums as a parameter and removes 
-all duplicates in the list. The function returns the modified 
+all duplicates in the list. The function returns the 
+modified 
 list.
 
 def remove_duplicates(nums):
@@ -10,13 +11,14 @@ Example Input: nums = [1,1,1,2,3,4,4,5,6,6]
 Example Output: no_dups = [1,2,3,4,5,6]'''
 
 
-def longest_uniform_substring(s):
-    pass
+def remove_duplicates(nums):
+    no=[]
+    for i in nums:
+        if i not in no:
+            no.append(i)
+    return no
 
-s1 = "aabbbbCdAA"
-l1 = longest_uniform_substring(s1)
-print(l1)
 
-s2 = "abcdef"
-l2 = longest_uniform_substring(s2)
-print(l2)
+nums = [1,1,1,2,3,4,4,5,6,6]
+nums =  ["extract of malt", "haycorns", "honey", "thistle", "thistle"]
+print(remove_duplicates(nums))
