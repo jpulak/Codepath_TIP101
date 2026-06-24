c777c6abe39926ab99676fb2b02a03a7c654a91e
Jana Pulak <jpulak@gmail.com>
2026-06-23 22:26:49 -0500

    s3-3

--- Unit 3/Session 2/V1 Problem set/prob3.py
+++ Unit 3/Session 2/V1 Problem set/prob3.py
@@ -1,5 +1,9 @@
 #Problem 3: Reverse Letters
-'''Write a function reverse_only_letters() that takes in a string s as a parameter. The function reverses the order of the letters in the string and returns the new string. Non-letter characters should remain in their original positions.
+'''Write a function reverse_only_letters() that 
+takes in a string s as a parameter. The function 
+reverses the order of the letters in the string and
+ returns the new string. Non-letter characters should
+   remain in their original positions.
 
 def reverse_only_letters(s):
     pass
@@ -11,3 +15,27 @@ print(reversed_s)
 Example Output: j-Ih-gfE-dCba
 
 '''
+
+def reverse_only_letters(s):
+    all =[]
+    for i in s:
+        if i.isalpha():
+            all.append(i)
+
+
+    res =""
+    end = len(all) -1
+    
+    for i in s:
+        if i.isalpha():
+            res += all[end]
+            end -= 1
+        else:
+            res += i
+    return res
+
+
+
+s = "a-bC-dEf-ghIj"
+reversed_s = reverse_only_letters(s)
+print(reversed_s)
--- Unit 4/Session 1/V1 Problem set/prob1.py
+++ Unit 4/Session 1/V1 Problem set/prob1.py
@@ -1,5 +1,9 @@
 #Problem 1: Prime Number
-'''Write a function is_prime() that takes in a positive integer n and returns True if it is a prime number and False otherwise. A prime number is a number greater than 1 that has no positive divisors other than 1 and itself.
+'''Write a function is_prime() that takes in a
+ positive integer n and returns True if it is a prime 
+ number and False otherwise. A prime number is a number 
+ greater than 1 that has no positive divisors other than
+   1 and itself.
 
 def is_prime(n):
     pass
@@ -13,3 +17,21 @@ Example Output:
 True
 False
 False'''
+
+'''
+understand is the integer is given if it is prime, return true 
+if not return false
+
+plan to figure out if it is a prime number
+
+'''
+def is_prime(n):
+    if n <1:
+        return False
+    i = 2
+    while i*i <=n:
+        
+
+print(is_prime(5))
+print(is_prime(12))
+print(is_prime(9))
