027487e926a78938f47f1893e5d5c0d451d7cbe9
Jana Pulak <jpulak@gmail.com>
2026-06-25 02:05:38 -0500

    s2-4

--- Unit 3/Session 2/V2 Problem set/prob4.py
+++ Unit 3/Session 2/V2 Problem set/prob4.py
@@ -1,5 +1,7 @@
 #Problem 4: Consecutive Characters
-'''Write a function count_consecutive_characters() that takes in a string str1 as a parameter and returns the count of the most frequent consecutive character.
+'''Write a function count_consecutive_characters() 
+that takes in a string str1 as a parameter and returns
+ the count of the most frequent consecutive character.
 
 def count_consecutive_characters(str1):
     pass
@@ -17,3 +19,31 @@ Example Output:
 1
 
 '''
+
+
+
+def count_consecutive_characters(str1):
+
+    if not str1:
+        return None
+    
+    most =1
+    track = 1
+    for i in range(1,len(str1)):
+        if str1[i] == str1[i-1]:
+            track += 1
+        else:
+            track = 1
+        most = max(most,track)
+    return most
+
+
+  
+
+str1 = "aaabbcaaaa"
+count = count_consecutive_characters(str1)
+print(count)
+
+str2 = "abcde"
+count2 = count_consecutive_characters(str2)
+print(count2)
