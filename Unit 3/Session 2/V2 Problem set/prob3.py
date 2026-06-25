6d0219d08169aa0152ed1c0a7798a0a57a12db2e
Jana Pulak <jpulak@gmail.com>
2026-06-25 01:12:32 -0500

    s2-3

--- Unit 3/Session 2/V2 Problem set/prob3.py
+++ Unit 3/Session 2/V2 Problem set/prob3.py
@@ -1,5 +1,9 @@
 #Problem 3: Longest Common Prefix
-'''Write a function longest_common_prefix() that takes in a list of strings strings as a parameter. The function returns the longest common prefix (not substring) of all strings and if there are none, it returns an empty string "".
+'''Write a function longest_common_prefix() that
+ takes in a list of strings strings as a parameter.
+   The function returns the longest common prefix 
+   (not substring) of all strings and if there are none,
+     it returns an empty string "".
 
 def longest_common_prefix(strings):
     pass
@@ -16,3 +20,32 @@ Example Output:
 
 fl
 '''
+'''
+find substring of all the strings not just one
+so compare for each in list
+
+compare the first of each list, then second then third
+'''
+
+def longest_common_prefix(strings):
+    if not strings:
+        return ""
+    
+
+    shortest= strings[0]
+    for s in strings:
+        if len(s) < len(shortest):
+            shortest =s
+    for i, char in enumerate(shortest):
+        for other in strings:
+            if other[i] != char:
+                return shortest[::i]
+    return shortest
+
+strings = ["flower", "flow", "flight"]
+common_string = longest_common_prefix(strings)
+print(common_string)
+
+strs = ["dog", "racecar", "car"]
+common_str = longest_common_prefix(strs)
+print(common_str)
