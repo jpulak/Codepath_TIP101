b557b4904b28cbe92e5f2854ee0c19f0e2174a53
Jana Pulak <jpulak@gmail.com>
2026-06-25 03:17:26 -0500

    s2-5

--- Unit 3/Session 2/V2 Problem set/prob5.py
+++ Unit 3/Session 2/V2 Problem set/prob5.py
@@ -1,5 +1,11 @@
 #Problem 5: Partition Labels
-'''Write a function partition_labels() that takes in a string s as a parameter. s consists of lowercase letters and represents the order of characters as they appear in a document. The function partitions s into as many parts as possible so that each unique letter appears in at most one part, and returns a list of integers representing the size of these parts.
+'''Write a function partition_labels() that takes in a 
+string s as a parameter. s consists of lowercase letters 
+and represents the order of characters as they appear in a
+ document. The function partitions s into as many parts as
+   possible so that each unique letter appears in at most 
+   one part, and returns a list of integers representing 
+   the size of these parts.
 
 def partition_label(s):
     pass
@@ -14,7 +20,30 @@ Example Output:
 
 # s1 partitioned into "ababcbaca", "defegde" and "hijhklij"
 [9, 7, 8]
-# s2 cannot be partitioned into more parts because of the "a" character at the end
+# s2 cannot be partitioned into more parts because of the 
+# "a" character at the end
 [16]
 
 '''
+def partition_label(s):
+    last_index = {}
+    for idx, char in enumerate(s):
+        last_index[char] = idx
+        
+    partitions = []
+    start = 0
+    end = 0
+
+    for i, char in enumerate(s):
+        end = max(end, last_index[char])
+        if i == end:
+            partitions.append(end - start + 1)
+            start = i + 1
+
+    return partitions
+
+s1 = "ababcbacadefegdehijhklij"
+print(partition_label(s1))
+
+s2 = "abcabcbadefffeda"
+print(partition_label(s2))
