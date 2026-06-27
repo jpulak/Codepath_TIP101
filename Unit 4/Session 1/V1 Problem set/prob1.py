565924f3bd8fe304e386d1366c44642a69ba0311
Jana Pulak <jpulak@gmail.com>
2026-06-26 23:54:22 -0500

    s1-1

--- Unit 4/Session 1/V1 Problem set/prob1.py
+++ Unit 4/Session 1/V1 Problem set/prob1.py
@@ -26,11 +26,15 @@ plan to figure out if it is a prime number
 
 '''
 def is_prime(n):
-    if n <1:
+    if n < 1:
         return False
     i = 2
     while i*i <=n:
-        
+        if n %i ==0:
+            return False
+        i +=1
+    return True
+
 
 print(is_prime(5))
 print(is_prime(12))
