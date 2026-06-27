298243f8b7dd0b5ce2b373aec885f2ecb18d4803
Jana Pulak <jpulak@gmail.com>
2026-06-27 17:58:52 -0500

    s1-3

--- Unit 4/Session 1/V1 Problem set/prob3.py
+++ Unit 4/Session 1/V1 Problem set/prob3.py
@@ -1,7 +1,11 @@
 #Problem 3: Evaluating Solutions
-'''The reverse_list() problem can also be solved without using the two pointer technique (as you may have seen it in previous units)! Evaluate the time and space complexity of your two-pointer solution.
+'''The reverse_list() problem can also be solved
+ without using the two pointer technique (as you may 
+ have seen it in previous units)! Evaluate the time and 
+ space complexity of your two-pointer solution.
 
-Then, evaluate the time and space of the following solution:
+Then, evaluate the time and space of the following 
+solution:
 
 def reverse_list(lst):
     # Create a new reversed list
@@ -12,3 +16,22 @@ def reverse_list(lst):
 Which has better time complexity?
 Which has better space complexity?
 '''
+
+def reverse_list(lst):
+    # Create a new reversed list
+    reversed_lst = lst[::-1]
+    # Copy the elements back into the original list
+    for i in range(len(lst)):
+        lst[i] = reversed_lst[i]
+
+'''# time complexity, O(n)
+# space complexity(O(n))
+
+#for the copy back using [::1] time is O(n) and space is 
+O(n) becuase it creates a new list
+
+for the two pointer, swap in place, time is also O(n)
+but space is more efficient O(1) because it only
+uses a few variables
+
+ '''
