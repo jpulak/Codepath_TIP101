e18fce9791d37a133a2dc6365c9d1bf5b5fb1615
Jana Pulak <jpulak@gmail.com>
2026-06-28 16:53:32 -0500

    s1-4

--- Unit 4/Session 1/V1 Problem set/prob4.py
+++ Unit 4/Session 1/V1 Problem set/prob4.py
@@ -1,5 +1,9 @@
 #Problem 4: Move Even Integers
-'''Write a function sort_list_by_parity() that takes in an integer list nums as a parameter and moves all the even integers at the beginning of the list followed by all the odd integers. The function returns any list that satisfies this condition.
+'''Write a function sort_list_by_parity() that takes
+ in an integer list nums as a parameter and moves all 
+ the even integers at the beginning of the list followed 
+ by all the odd integers. The function returns any list 
+ that satisfies this condition.
 
 def sort_array_by_parity(nums):
     pass
@@ -12,6 +16,38 @@ print(sort_array_by_parity(nums2))
 Example Output:
 
 [2,4,3,1]
-# Additional acceptable outputs are [4,2,3,1], [2,4,1,3], and [4,2,1,3]
+# Additional acceptable outputs are [4,2,3,1], 
+# [2,4,1,3], and [4,2,1,3]
 [0]
 '''
+
+'''
+i need to two pointers to go theroughte hlist, 
+one pointer is at the first of the list and the seocnd poitner
+scans the list until it finda n even number
+then it goes to the secon dpositon and then keep sscanning for the
+next even number
+'''
+
+def sort_array_by_parity(nums):
+    left = 0
+    right= len(nums) -1
+
+    while left < right:
+        if nums[left]%2 ==0:
+            left +=1
+        elif nums[right]%2 != 0:
+            right -=1
+        else: # nums[left]%2 != 0 and nums[right] %2 ==0:
+            nums[left], nums[right]= nums[right], nums[left]
+            left +=1
+            right -=1
+    return nums
+
+
+
+nums = [3,1,2,4]
+print(sort_array_by_parity(nums))
+
+# nums2 = [0]
+# print(sort_array_by_parity(nums2))
