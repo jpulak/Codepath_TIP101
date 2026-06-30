3f9ec58815c653ba7465e83ac06cd9b24fd7e525
Jana Pulak <jpulak@gmail.com>
2026-06-30 02:02:03 -0500

    s2-1

--- Unit 4/Session 1/V2 Problem set/prob1.py
+++ Unit 4/Session 1/V2 Problem set/prob1.py
@@ -1,7 +1,13 @@
 #Problem 1: Perfect Number
-'''Write a function is_perfect_number() that takes in a positive integer n and returns True if it is a perfect number and False otherwise. A perfect number is a positive integer that is equal to the sum of its proper divisors, excluding itself.
+'''Write a function is_perfect_number() that 
+takes in a positive integer n and returns True 
+if it is a perfect number and False otherwise.
+ A perfect number is a positive integer that is
+   equal to the sum of its proper divisors, 
+   excluding itself.
 
-For example, 6 is a perfect number because its divisors or 1, 2, and 3 and 1 + 2 + 3 = 6.
+For example, 6 is a perfect number because its 
+divisors or 1, 2, and 3 and 1 + 2 + 3 = 6.
 
 def is_perfect_number(n):
     pass
@@ -16,3 +22,20 @@ True
 True
 False
 '''
+
+def is_perfect_number(n):
+    if n<=1:
+        return False
+    
+    total = 0
+    i=1
+    #Loop through numbers from 1 up to n/2
+    while i<= n //2:
+        if n%i == 0:
+            total += i
+        i +=1
+    return total ==n
+
+print(is_perfect_number(6))
+print(is_perfect_number(28))
+print(is_perfect_number(9))
