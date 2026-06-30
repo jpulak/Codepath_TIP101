43275b8321cc22dbebae7eb602908caa8221e8c9
Jana Pulak <jpulak@gmail.com>
2026-06-30 02:04:48 -0500

    s2-2

--- Unit 4/Session 1/V2 Problem set/prob2.py
+++ Unit 4/Session 1/V2 Problem set/prob2.py
@@ -1,7 +1,21 @@
 #Problem 2: 2-Pointer Palindrome
-'''Write a function is_palindrome() that takes in a string s as a parameter and returns True if the string is a palindrome and False otherwise. You may assume the string contains only lowercase alphabetic characters.
+'''Write a function is_palindrome() that takes in a 
+string s as a parameter and returns True if the string
+ is a palindrome and False otherwise. You may assume the
+   string contains only lowercase alphabetic characters.
 
-The function must use the two-pointer approach, which is a common technique in which we initialize two variables (also called a pointer in this context) to track different indices or places in a list or string, then moves the pointers to point at new indices based on certain conditions. In the most common variation of the two-pointer approach, we initialize one variable to point at the beginning of a list and a second variable/pointer to point at the end of list. We then shift the pointers to move inwards through the list towards each other, until our problem is solved or the pointers reach the opposite ends of the list.
+The function must use the two-pointer approach, which is 
+a common technique in which we initialize two variables 
+(also called a pointer in this context) to track different 
+indices or places in a list or string, then moves the 
+pointers to point at new indices based on certain 
+conditions. In the most common variation of the
+two-pointer approach, we initialize one variable 
+to point at the beginning of a list and a second 
+variable/pointer to point at the end of list. We then
+ shift the pointers to move inwards through the list 
+ towards each other, until our problem is solved or 
+ the pointers reach the opposite ends of the list.
 
 def is_palindrome(s):
     pass
@@ -15,5 +29,26 @@ print(is_palindrome(s2))
 Example Output:
 
 True
-False
+Fals
 '''
+'''
+intialize two variagle, one to the left, another ot he right
+and compare if they are the same or different
+fi differnt return false
+if it goes through all return treu
+'''
+def is_palindrome(s):
+    left = 0
+    right = len(s) -1
+    while left < right:
+        if s[left] != s[right]:
+            return False
+        left +=1
+        right -=1
+    return True
+
+s = "amanaplanacanalpanama"
+s2 = "helloworld"
+
+print(is_palindrome(s))
+print(is_palindrome(s2))
