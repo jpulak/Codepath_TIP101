95d0284d9d8b66bcea403f9a11403e605009e0d0
Jana Pulak <jpulak@gmail.com>
2026-07-02 01:00:47 -0500

    s3-5

--- Unit 4/Session 1/V3 Problem set/prob5.py
+++ Unit 4/Session 1/V3 Problem set/prob5.py
@@ -1,5 +1,10 @@
 #Problem 5: Reverse Prefix
-'''Write a function reverse_prefix() that takes in a 0-indexed string word and a character ch as parameters. The function reverses the segment of word that starts at index 0 and ends at the index of the first occurrence of ch (inclusive) and keeps the rest of the string the same. If ch does not exist in word, do nothing. Return the resulting string.
+'''Write a function reverse_prefix() that takes in a 
+0-indexed string word and a character ch as parameters. 
+The function reverses the segment of word that starts at index 
+0 and ends at the index of the first occurrence of ch 
+(inclusive) and keeps the rest of the string the same. 
+If ch does not exist in word, do nothing. Return the resulting string.
 
 def reverse_prefix(word, ch):
     pass
@@ -22,4 +27,38 @@ dcbaefd
 wollehorld
 xyzxyz
 
+
+iterate through string, fi it matches with the ch
+swap wiht the 0 index
 '''
+
+def reverse_prefix(word, ch):
+    word = [*word]
+    if ch in word:
+        right = word.index(ch)
+    else:
+        right = -1
+
+    if right == -1:
+        return "".join(word)
+
+    left = 0
+    while left< right:
+        word[left],word[right] = word[right], word[left]
+        left +=1
+        right-=1
+        
+    return "".join(word)
+
+word = "abcdefd"
+rev_word = reverse_prefix(word, "d")
+print(rev_word)
+
+word2 = "helloworld"
+rev_word2 = reverse_prefix(word2, "w")
+print(rev_word2)
+
+word3 = "xyzxyz"
+rev_word3 = reverse_prefix(word3, "a")
+print(rev_word3)
+
