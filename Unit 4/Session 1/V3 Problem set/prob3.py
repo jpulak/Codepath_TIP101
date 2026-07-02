2b16a7e48bade9d6dd3671d5d37916e4d4293d5a
Jana Pulak <jpulak@gmail.com>
2026-07-01 23:46:56 -0500

    s3-3

--- Unit 4/Session 1/V3 Problem set/prob3.py
+++ Unit 4/Session 1/V3 Problem set/prob3.py
@@ -14,3 +14,31 @@ def two_sum(nums, target):
 Which has better time complexity?
 Which has better space complexity?
 '''
+
+# NON TWO POINTER
+# Time complexity: O(n)
+# Space complexity: O(n)
+def two_sum(nums, target):
+    prev_map = {}  # O(n) space complexit
+    
+    for i in range(len(nums)): # O(n) time complexity
+        diff = target - nums[i]
+        if diff in prev_map:
+            return [prev_map[diff], i]
+        prev_map[nums[i]] = i
+        
+# TWO POINTER
+# Time complexity: O(n)
+# Space complexity: O(1)
+def two_sum(nums, target):
+    left = 0
+    right = len(nums) - 1
+    
+    while left < right: # O(n) time complexity
+        current_sum = nums[left] + nums[right]
+        if current_sum == target:
+            return [left, right] 
+        elif current_sum < target:
+            left += 1  # Need a larger sum
+        else:
+            right -= 1  # Need a smaller sum  
