9b45a50acc564edb6ea782990c8afc73aff420a0
Jana Pulak <jpulak@gmail.com>
2026-07-01 22:38:18 -0500

    s3-1

--- Unit 4/Session 1/V3 Problem set/prob1.py
+++ Unit 4/Session 1/V3 Problem set/prob1.py
@@ -1,5 +1,8 @@
 #Problem 1: Highest Exponent
-'''Write a function find_highest_exponent() that takes in an integer base and an integer limit as parameters. The function returns the highest exponent for which a given base raised to that exponent is less than or equal to limit.
+'''Write a function find_highest_exponent() that takes in an 
+integer base and an integer limit as parameters. The function 
+returns the highest exponent for which a given base raised to 
+that exponent is less than or equal to limit.
 
 def find_highest_exponent(base, limit):
     pass
@@ -16,3 +19,18 @@ Example Output:
 6
 # 3^1 = 3 and 3^2 = 9
 1'''
+
+def find_highest_exponent(base, limit):
+    expo = 0
+    power =1 
+    while power *base <= limit:
+        power *= base
+        expo +=1
+    return expo
+     
+
+exp = find_highest_exponent(2, 100)
+print(exp)
+
+exp2 = find_highest_exponent(3, 5)
+print(exp2)
