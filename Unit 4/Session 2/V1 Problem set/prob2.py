9c5e7e0b34e7e9835ce6007187928652003b191d
Jana Pulak <jpulak@gmail.com>
2026-07-03 02:23:24 -0500

    s1-2

--- Unit 4/Session 2/V1 Problem set/prob2.py
+++ Unit 4/Session 2/V1 Problem set/prob2.py
@@ -1,11 +1,16 @@
 #Problem 2: Sharing Cookies
-'''Imagine you're an awesome babysitter and want to give the kids you're looking after some cookies as a snack.
-Each child i has a greed factor g[i], which is the minimum size of a cookie that the child will be content with.
+'''Imagine you're an awesome babysitter and want to give the kids
+ you're looking after some cookies as a snack.
+Each child i has a greed factor g[i], which is the minimum size of
+ a cookie that the child will be content with.
 Each cookie j has a cookie size s[j].
-If s[j] >= g[i], we can assign the cookie j to the child i, and the child will be content.
+If s[j] >= g[i], we can assign the cookie j to the child i, and the 
+child will be content.
 If s[j] < g[i], the child will not be content.
 
-Write a function find_content_children() that takes in the greed list g and the cookie size list s as parameters and maximizes the number of content children you are babysitting! The function returns
+Write a function find_content_children() that takes in the greed list 
+g and the cookie size list s as parameters and maximizes the number of 
+content children you are babysitting! The function returns
 
 Example 1:
 
@@ -36,3 +41,4 @@ s = [2,2,2]
 
 find_content_children(s, g) 
 # Output: 2 '''
+
