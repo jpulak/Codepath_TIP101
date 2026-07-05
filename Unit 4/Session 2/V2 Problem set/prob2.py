bc657569860ddd6486a8392715e1d25d99fed88b
Jana Pulak <jpulak@gmail.com>
2026-07-05 11:22:47 -0500

    s2-2

--- Unit 4/Session 2/V2 Problem set/prob2.py
+++ Unit 4/Session 2/V2 Problem set/prob2.py
@@ -1,7 +1,12 @@
 #Problem 2: Checking Subsequence
-'''Write a function is_subsequence that takes in two strings s and t as parameters and returns True if s is a subsequence of t and False otherwise.
+'''Write a function is_subsequence that takes in 
+two strings s and t as parameters and returns True if s is 
+a subsequence of t and False otherwise.
 
-A subsequence of a string is a new string that is formed from the original string by deleting some or none of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+A subsequence of a string is a new string that is formed from 
+the original string by deleting some or none of the characters 
+without disturbing the relative positions of the remaining characters. 
+(i.e., "ace" is a subsequence of "abcde" while "aec" is not).
 
 def is_subsequence(s, t):
 	pass
@@ -18,3 +23,19 @@ Example Output:
 
 True
 False'''
+def is_subsequence(s, t):
+	sindex, tindex = 0,0
+
+	while sindex < len(s) and tindex < len(t):
+		if s[sindex] == t[tindex]:
+			sindex +=1
+		tindex +=1
+	return sindex == len(s) #if the string is the same length
+
+s = "abc"
+t = "ahbgdc"
+print(is_subsequence(s, t))
+
+a = "cba"
+b = "ahbgdc"
+print(is_subsequence(a, b))
