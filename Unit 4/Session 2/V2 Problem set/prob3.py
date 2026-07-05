5dec25986b03f8b55c1b9a2c157a33df7ea2b8b5
Jana Pulak <jpulak@gmail.com>
2026-07-05 11:44:47 -0500

    s2-3

--- Unit 4/Session 2/V2 Problem set/prob3.py
+++ Unit 4/Session 2/V2 Problem set/prob3.py
@@ -1,5 +1,10 @@
 #Problem 3: Sort List by Parity
-'''Write a function sort_array_by_parity() that takes in a list of integers nums where half of the integers are odd, and the other half are even. The function sorts the list so that whenever nums[i] is odd, i is odd and whenever nums[i] is even, i is even. The function returns the list in any order that satisfies the condition.
+'''Write a function sort_array_by_parity() that takes in a
+ list of integers nums where half of the integers are odd, 
+ and the other half are even. The function sorts the list so
+   that whenever nums[i] is odd, i is odd and whenever nums[i] is 
+   even, i is even. The function returns the list in any order 
+   that satisfies the condition.
 
 def sort_array_by_parity(nums):
 	pass
@@ -16,3 +21,45 @@ Example Output:
 [2,3]
 
 '''
+
+def sort_array_by_parity(nums):
+	n = len(nums)
+	evenPointer, oddPointer = 0, 1  # Starting indices for even and odd
+
+	while evenPointer < n and oddPointer < n:
+		# Find the next even number that is out of place
+		while evenPointer < n and nums[evenPointer] % 2 == 0:
+			evenPointer += 2
+		# Find the next odd number that is out of place
+		while oddPointer < n and nums[oddPointer] % 2 == 1:
+			oddPointer += 2
+		
+		# If neither pointer has reached the end, swap their elements
+		if evenPointer < n and oddPointer < n:
+			nums[evenPointer],nums[oddPointer]=nums[oddPointer],nums[evenPointer]
+	return nums
+
+# def sort_array_by_parity(nums):
+# 	check = 0
+# 	new = 0
+# 	while check < len(nums):
+# 		if check%2 == 0:
+# 			new = 0
+# 			while new < len(nums):
+# 				if nums[new] %2 ==0:
+# 					nums[check],nums[new] = nums[new], nums[check]
+# 				new +=1
+# 		else:
+# 			new = 0
+# 			while new < len(nums):
+# 				if nums[new] %2 !=0:
+# 					nums[check],nums[new] = nums[new], nums[check]
+# 				new +=1
+# 		check +=1
+
+# 	return nums
+nums = [4,2,5,7]
+nums2 = [2,3]
+print(sort_array_by_parity(nums))
+print(sort_array_by_parity(nums2))
+
