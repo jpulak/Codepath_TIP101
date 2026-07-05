c30c5afd6dea95082323356eb8cf039b35ba4584
Jana Pulak <jpulak@gmail.com>
2026-07-05 12:22:44 -0500

    s3-2

--- Unit 4/Session 2/V3 Problem set/prob2.py
+++ Unit 4/Session 2/V3 Problem set/prob2.py
@@ -1,5 +1,9 @@
 #Problem 2: Count Pairs
-'''Write a function count_pairs() that takes in a 0-indexed list of integers nums of length n and an integer target as parameters. The function returns the number of index pairs (i, j) where 0 <= i < j < n and nums[i] + nums[j] < target.
+'''Write a function count_pairs() that takes in a
+ 0-indexed list of integers nums of length n and 
+ an integer target as parameters. The function
+   returns the number of index pairs (i, j) 
+   where 0 <= i < j < n and nums[i] + nums[j] < target.
 
 def count_pairs(nums, target):
 	pass
@@ -28,3 +32,22 @@ Example Output:
 # nums[4] + nums[5] = -8
 # nums[4] + nums[6] = -4
 10'''
+
+def count_pairs(nums, target):
+	nums.sort() #sort array using two pointer
+
+	count = 0
+	n = len(nums)
+
+	for left in range(n):
+		right = left +1
+		while right < n and nums[left] + nums[right] < target:
+			right +=1
+		count += right -left -1 #add number of valid paris with nums[left]
+	return count
+
+nums = [-1,1,2,3,1]
+print(count_pairs(nums,2))
+
+nums2 = [-6,2,5,-2,-7,-1,3]
+print(count_pairs(nums2,2))
