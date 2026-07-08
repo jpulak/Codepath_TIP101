3b955377fb5ac14b56635ad71bc4c5bac340ecef
Jana Pulak <jpulak@gmail.com>
2026-07-08 16:30:26 -0500

    s1-11

--- Unit 5/Session 1/V1 Problem Set/prob11.py
+++ Unit 5/Session 1/V1 Problem Set/prob11.py
@@ -1,5 +1,6 @@
 #Problem 11: Mario Party
-'''Create the list ["Mario", "Luigi", "Wario", "Toad"] as a linked list given the Node class:
+'''Create the list ["Mario", "Luigi", "Wario", "Toad"] 
+as a linked list given the Node class:
 
 class Node:
 	def __init__(self, value, next=None):
@@ -7,7 +8,8 @@ class Node:
 		self.next = next
 Result Linked List would be: Mario -> Luigi -> Wario -> Toad
 
-Example Usage (after completing the problem with variable names node_1, node_2, node_3, and node_4.):
+Example Usage (after completing the problem with variable 
+names node_1, node_2, node_3, and node_4.):
 
 print(node_1.value, "->", node_1.next.value)
 print(node_2.value, "->", node_2.next.value)
@@ -20,3 +22,24 @@ Luigi -> Wario
 Wario -> Toad
 Toad -> None
 '''
+class Node:
+	def __init__(self, value, next=None):
+		self.value = value
+		self.next = next
+
+node_1 = Node('Mario')
+node_2 = Node('Luigi')
+node_3 = Node('Wario')
+node_4 = Node('Toad')
+
+# node_1.next = node_2
+# node_2.next = node_3
+# node_3.next = node_4
+
+head = Node('Mario',Node('Luigi',Node('Wario',Node('Toad'))))
+
+
+print(node_1.value, "->", node_1.next.value)
+print(node_2.value, "->", node_2.next.value)
+print(node_3.value, "->", node_3.next.value)
+print(node_4.value, "->", node_4.next)
