9a9980831ae5e68acfa2c58011f94c0cee310b22
Jana Pulak <jpulak@gmail.com>
2026-07-08 16:16:25 -0500

    s1-10

--- Unit 5/Session 1/V1 Problem Set/prob10.py
+++ Unit 5/Session 1/V1 Problem Set/prob10.py
@@ -1,7 +1,10 @@
 #Problem 10: Linking Nodes
-'''Building off the Node class from Problem 9, now we will link the nodes together.
+'''Building off the Node class from Problem 9,
+ now we will link the nodes together.
 
-To link the nodes, we can set a node's next attribute to hold another node. Update node_one from the Problem 9 to point to node_two.
+To link the nodes, we can set a node's next attribute 
+to hold another node. Update node_one from the Problem 9 to 
+point to node_two.
 
 Example Usage (after updating node_one's next property):
 
@@ -14,3 +17,17 @@ a
 b
 b
 '''
+
+class Node:
+	def __init__(self, value, next=None):
+		self.value = value
+		self.next = next
+
+node_one = Node('a')
+node_two = Node('b')
+
+node_one.next = node_two
+
+print(node_one.value)
+print(node_one.next.value)
+print(node_two.value)
