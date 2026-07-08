c83dcc6a003718a8b3449e6d575beec8f5eecb9e
Jana Pulak <jpulak@gmail.com>
2026-07-08 14:54:40 -0500

    s1-5

--- Unit 5/Session 1/V1 Problem Set/prob5.py
+++ Unit 5/Session 1/V1 Problem Set/prob5.py
@@ -1,9 +1,12 @@
 #Problem 5: Choose Pokemon
-'''Update the Pokemon class with a new method choose() that takes in no parameters except self.
+'''Update the Pokemon class with a new method choose() 
+that takes in no parameters except self.
 
-If the Pokemon is caught, the method should print the string "<Pokemon name> I choose you!".
+If the Pokemon is caught, the method should print the string 
+"<Pokemon name> I choose you!".
 
-Otherwise, it should print "<Pokemon name> is wild! Catch them if you can!".
+Otherwise, it should print "<Pokemon name> is wild! Catch them 
+if you can!".
 
 class Pokemon():
 	...
@@ -23,3 +26,29 @@ Example Output:
 {'name': 'rattata', 'types': ['Normal'], 'is_caught': False}
 rattata is wild! Catch them if you can!
 rattata I choose you!'''
+
+class Pokemon:
+	def __init__(self, name, types):
+		self.name = name
+		self.types = types
+		self.is_caught = False
+
+	def print_pokemon(self):
+		print(f"Name :{self.name} Type: {self.types} Status: {self.is_caught}")
+	
+	def catch(self):
+		self.is_caught = True
+	
+	def choose(self):
+		if self.is_caught == True:
+			print(f"{self.name} I choose you!")
+		else:
+			print(f"{self.name} is wild! Catch them if you can!")
+        
+
+my_pokemon = Pokemon("rattata", ["Normal"])
+my_pokemon.print_pokemon()
+
+my_pokemon.choose()
+my_pokemon.catch()
+my_pokemon.choose()
