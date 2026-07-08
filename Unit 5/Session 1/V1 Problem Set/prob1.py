b257e7d697f751043e08ee5e8c725fa7a0787b16
Jana Pulak <jpulak@gmail.com>
2026-07-08 14:26:58 -0500

    s1-1

--- Unit 5/Session 1/V1 Problem Set/prob1.py
+++ Unit 5/Session 1/V1 Problem Set/prob1.py
@@ -13,3 +13,13 @@ class Pokemon:
         self.types = types m
         self.is_caught = False'''
 
+class Pokemon:
+    def __init__(self, name, types):
+        self.name = name
+        self.types = types
+        self.is_caught = False
+
+my_pokemon = Pokemon("Pikachu",["Electric"])
+
+print(my_pokemon.name)
+print(my_pokemon.types)
