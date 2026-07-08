10b85c6c840a33726a28a96dec8231b08deab4fc
Jana Pulak <jpulak@gmail.com>
2026-07-08 14:43:29 -0500

    s1-3

--- Unit 5/Session 1/V1 Problem Set/prob3.py
+++ Unit 5/Session 1/V1 Problem Set/prob3.py
@@ -1,5 +1,8 @@
 #Problem 3: Is Caught
-'''Using your code from Problem 2, update your squirtle Pokemon so that is_caught is updated to True. Use the print_pokemon() function to verify that squirtle's is_caught property was updated.
+'''Using your code from Problem 2, update your 
+squirtle Pokemon so that is_caught is updated to 
+True. Use the print_pokemon() function to verify 
+that squirtle's is_caught property was updated.
 
 Expected Output:
 
@@ -11,3 +14,20 @@ Expected Output:
 Example Output
 
 {'name': 'Squirtle', 'types': ['water'], 'is_caught': True}'''
+
+class Pokemon:
+    def __init__(self, name, types):
+        self.name = name
+        self.types = types
+        self.is_caught = False
+
+    def print_pokemon(self):
+        print({
+            "name": self.name,   
+            "types": self.types, 
+            "is_caught": self.is_caught 
+        })
+
+squirtle = Pokemon("Squirtle", ["water"])
+squirtle.is_caught = False
+squirtle.print_pokemon()
