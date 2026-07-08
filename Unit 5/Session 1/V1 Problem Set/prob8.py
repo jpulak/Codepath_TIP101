d4b2d2a7c70245b9b0b0a76b8fa621505c8c442a
Jana Pulak <jpulak@gmail.com>
2026-07-08 16:02:34 -0500

    s1-8

--- Unit 5/Session 1/V1 Problem Set/prob8.py
+++ Unit 5/Session 1/V1 Problem Set/prob8.py
@@ -1,9 +1,14 @@
 #Problem 8: Pokemon Evolution
-'''Some Pokemon can evolve into other species of Pokemon. In the updated Pokemon class below, each instance of Pokemon has an attribute evolution. The attribute will either be the default value of None or another Pokemon instance.
+'''Some Pokemon can evolve into other species of Pokemon. 
+In the updated Pokemon class below, each instance of Pokemon 
+has an attribute evolution. The attribute will either be the
+ default value of None or another Pokemon instance.
 
-Write a function get_evolutionary_line() that takes in a Pokemon object starter_pokemon as a parameter.
+Write a function get_evolutionary_line() that takes in a 
+Pokemon object starter_pokemon as a parameter.
 
-The function should return a list of itself and the Pokemon that the starter_pokemon can evolve into.
+The function should return a list of itself and the Pokemon 
+that the starter_pokemon can evolve into.
 
 class Pokemon():
 	def  __init__(self, name, types, evolution = None):
@@ -35,3 +40,32 @@ Example Output:
 ['Charizard']
 
 '''
+
+class Pokemon():
+	def  __init__(self, name, types, evolution = None):
+		self.name = name
+		self.types = types
+		self.is_caught = False
+		self.evolution = evolution
+ 
+def get_evolutionary_line(starter_pokemon):
+	line = []
+	current = starter_pokemon
+
+	while current:
+		line.append(current.name)
+		current = current.evolution
+	return line
+
+charizard = Pokemon("Charizard", ["fire", "flying"])
+charmeleon = Pokemon("Charmeleon", ["fire"], charizard)
+charmander = Pokemon("Charmander", ["fire"], charmeleon)
+
+charmander_list = get_evolutionary_line(charmander)
+print(charmander_list)
+
+charmeleon_list = get_evolutionary_line(charmeleon)
+print(charmeleon_list)
+
+charizard_list = get_evolutionary_line(charizard)
+print(charizard_list)
