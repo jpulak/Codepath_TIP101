40f628f3ffc04027d126f8a047d4a935fc1c9161
Jana Pulak <jpulak@gmail.com>
2026-07-08 14:53:54 -0500

    s1-4

--- Unit 5/Session 1/V1 Problem Set/prob4.py
+++ Unit 5/Session 1/V1 Problem Set/prob4.py
@@ -1,7 +1,9 @@
 #Problem 4: Catch Pokemon
-'''Update the Pokemon class with a new method catch() that takes in no parameters except self.
+'''Update the Pokemon class with a new method 
+catch() that takes in no parameters except self.
 
-The method should update the Pokemon's is_caught attribute to True and not return any value.
+The method should update the Pokemon's is_caught 
+attribute to True and not return any value.
 
 class Pokemon():
 	...
@@ -17,5 +19,27 @@ my_pokemon.catch()
 my_pokemon.print_pokemon()
 Example Output:
 
-{'name': 'rattata', 'types': ['Normal'], 'is_caught': False} # First print statement
-{'name': 'rattata', 'types': ['Normal'], 'is_caught': True}  # Second print statement'''
+{'name': 'rattata', 'types': ['Normal'], 'is_caught': False} 
+# First print statement
+{'name': 'rattata', 'types': ['Normal'], 'is_caught': True} 
+ # Second print statement'''
+
+class Pokemon:
+	def __init__(self, name, types):
+		self.name = name
+		self.types = types
+		self.is_caught = False
+
+	def print_pokemon(self):
+		print(f"Name :{self.name} Type: {self.types} Status: {self.is_caught}")
+	
+	def catch(self):
+		self.is_caught = True
+	
+        
+
+my_pokemon = Pokemon("rattata", ["Normal"])
+my_pokemon.print_pokemon()
+
+my_pokemon.catch()
+my_pokemon.print_pokemon()
