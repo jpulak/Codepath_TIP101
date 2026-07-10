9f8c82cbb965ca82065c2c6bfd2673c4e53c28ae
Jana Pulak <jpulak@gmail.com>
2026-07-10 11:20:46 -0500

    s2-4

--- Unit 5/Session 1/V2 Problem Set/prob4.py
+++ Unit 5/Session 1/V2 Problem Set/prob4.py
@@ -26,3 +26,25 @@ Example Output:
 7
 11
 '''
+
+class Card():
+	def  __init__(self, suit, rank):
+		self.suit = suit
+		self.rank = rank
+
+	def print_card(self):
+		print(f"{self.rank} of {self.suit}")
+
+	def is_valid(self):
+		suit = ["Hearts", "Spades", "Clubs", "Diamonds"]
+		rank = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
+
+		if self.suit in suit and self.rank in rank:
+			return True
+		return False
+	
+my_card = Card("Hearts", "7")
+print(my_card.is_valid())
+
+second_draw = Card("Spades", "Joker")
+print(second_draw.is_valid())
