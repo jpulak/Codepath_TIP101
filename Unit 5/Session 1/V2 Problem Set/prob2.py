891f75f87bfa4f34b235f0d4c8dd25e9277076ac
Jana Pulak <jpulak@gmail.com>
2026-07-10 10:58:54 -0500

    s2-2

--- Unit 5/Session 1/V2 Problem Set/prob2.py
+++ Unit 5/Session 1/V2 Problem Set/prob2.py
@@ -1,11 +1,23 @@
 #Problem 2: Print Card
-'''Step 1: Update the Card class with the new method print_card() provided below:
+'''Step 1: Update the Card class with the new method 
+print_card() provided below:
 
 def print_card(self):
 	print(f"{self.rank} of {self.suit}")
-Step 2: Create an instance of the class and store it in a variable named card. The object should have suit "Clubs" and rank "Ace".
+Step 2: Create an instance of the class and store it in a 
+variable named card. The object should have suit "Clubs" and rank "Ace".
 
 Step 3: Then, call the method print_card() on your card.
 
 Expected Output: Ace of Clubs
 '''
+
+class Card():
+	def  __init__(self, suit, rank):
+		self.suit = suit
+		self.rank = rank
+
+	def print_card(self):
+		print(f"Suit: {self.suit} Rank: {self.rank}")
+new = Card("Spades","8")
+new.print_card()
