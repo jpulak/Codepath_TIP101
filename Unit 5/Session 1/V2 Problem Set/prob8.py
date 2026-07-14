b5a80418b8154aa47fc224c3aeb1b5568a671351
Jana Pulak <jpulak@gmail.com>
2026-07-14 13:33:06 -0500

    s2-8

--- Unit 5/Session 1/V2 Problem Set/prob8.py
+++ Unit 5/Session 1/V2 Problem Set/prob8.py
@@ -22,3 +22,12 @@ card_two.next = card_three
 
 print_hand(card_one)
 Example Output: [card_one, card_two, card_three]'''
+
+class Card():
+	def  __init__(self, suit, rank, next = None):
+		self.suit = suit
+		self.rank = rank
+		self.next = next
+		
+def print_hand(starting_card):
+	pass
