67f97b7a21d860a0e4c0bd5a76c6549b6620ced3
Jana Pulak <jpulak@gmail.com>
2026-07-17 18:41:46 -0500

    s2

--- Unit 5/Session 1/V2 Problem Set/prob12.py
+++ Unit 5/Session 1/V2 Problem Set/prob12.py
@@ -1,6 +1,6 @@
 #Problem 12: Print Linked List
 '''Write a function print_linked_list() that takes in a head of a linked list as a parameter. The function prints and returns a list of all the values in the linked list in the order they appear in the linked list.
-
+asdf
 Note: The "head" of a linked list is the first element in the linked list, equivalent to lst[0] of a normal list.
 
 class Node:
