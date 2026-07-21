b47c64252284854db9d36a95e00db5817c12059d
Jana Pulak <jpulak@gmail.com>
2026-07-21 17:58:16 -0500

    s2

--- Unit 5/Session 1/V2 Problem Set/prob6.py
+++ Unit 5/Session 1/V2 Problem Set/prob6.py
@@ -32,4 +32,51 @@ player1_hand.add_card(card_two)
 player1_hand.remove_card(card_one)
 # cards = [card_two]
 '''
+class Card():
+    def  __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
 
+    def print_card(self):
+        print(f"{self.rank} of {self.suit}")
+
+    def is_valid(self):
+        suit = ["Hearts", "Spades", "Clubs", "Diamonds"]
+        rank = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
+
+    def get_value(self):
+        if self.rank == "Ace":
+            return 1
+        elif self.rank == "Jack":
+            return 11
+        elif self.rank == "Queen":
+            return 12
+        elif self.rank == "King":
+            return 13
+        elif self.rank.isalnum():
+            return self.rank
+        
+class Hand:
+    def __init__(self):
+        self.cards = []
+        
+    def add_card(self, card):
+        pass
+        
+    def remove_card(self, card):
+        pass
+
+card_one = Card("Hearts", "3")
+card_two = Card("Spades", "8")
+
+player1_hand = Hand()
+cards = []
+
+player1_hand.add_card(card_one)
+cards = [card_one]
+
+player1_hand.add_card(card_two)
+cards = [card_one, card_two]
+
+player1_hand.remove_card(card_one)
+cards = [card_two]
