9e9d4165ee2b7b94b52972b92fa6cf85830b0ae4
Jana Pulak <jpulak@gmail.com>
2026-07-21 17:58:23 -0500

    s2

--- Unit 8/Session 1/V1 Problem Set/prob1.py
+++ Unit 8/Session 1/V1 Problem Set/prob1.py
@@ -8,3 +8,9 @@ class TreeNode:
         self.val = val
         self.left = left
         self.right = right'''
+
+class TreeNode:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
