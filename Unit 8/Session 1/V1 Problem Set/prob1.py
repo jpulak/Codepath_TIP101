41ddd392cc557bae4d3bbe9652e6f7443f22195c
Jana Pulak <jpulak@gmail.com>
2026-07-23 18:51:57 -0500

    s1-1

--- Unit 8/Session 1/V1 Problem Set/prob1.py
+++ Unit 8/Session 1/V1 Problem Set/prob1.py
@@ -9,8 +9,38 @@ class TreeNode:
         self.left = left
         self.right = right'''
 
-class TreeNode:
-    def __init__(self, val, left=None, right=None):
-        self.val = val
+class TreeNode():
+    def __init__(self, value, left=None, right=None):
+        self.val = value
         self.left = left
         self.right = right
+         
+
+def is_univalued_help(node, value):
+    if not node:
+        return True
+
+    if node.val != value:
+        return False
+    
+    return is_univalued_help(node.left, value) and is_univalued_help(node.right, value)
+
+def if_univalued(root):
+    if not root:
+        return True
+
+    return is_univalued_help(root,root.val)
+
+
+root1 = TreeNode(1,
+    TreeNode(1,
+        TreeNode(1),
+        TreeNode(1)
+    ),
+    TreeNode(1,
+        None,
+        TreeNode(1)
+    )
+)
+
+print(if_univalued(root1))
