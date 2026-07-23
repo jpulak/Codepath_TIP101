fcdebb203811b931ecbf9d7ac60dfa614124686a
Jana Pulak <jpulak@gmail.com>
2026-07-23 00:58:15 -0500

    s1-2

--- Unit 7/Session 1/V1 Problem Set/prob2.py
+++ Unit 7/Session 1/V1 Problem Set/prob2.py
@@ -14,3 +14,11 @@ Example Output:
 
 # Expected Output: 120
 # Explanation: 5! = 5 * 4 * 3 * 2 * 1 = 120'''
+
+def factorial(n):
+	if n == 0:
+		return 1
+	else:
+		return n* factorial(n-1)
+	
+print(factorial(5))
