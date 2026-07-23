7a23cae0c2ddc68a3e950bcb181d9f3e09b62b8f
Jana Pulak <jpulak@gmail.com>
2026-07-23 01:03:06 -0500

    s1-3

--- Unit 7/Session 1/V1 Problem Set/prob3.py
+++ Unit 7/Session 1/V1 Problem Set/prob3.py
@@ -13,3 +13,12 @@ Example Output:
 # Expected Output: 15
 # Explanation: 1 + 2 + 3 + 4 + 5 = 15
 '''
+
+def sum_list(lst):
+	if len(lst) ==0:
+		return 0
+	else:
+		return lst[0] + sum_list(lst[1:])
+	
+
+print(sum_list([1, 2, 3, 4, 5]))
