ac8583b66ad554a343f4b71f984a034acb239056
Jana Pulak <jpulak@gmail.com>
2026-07-24 00:42:53 -0500

    s1-5

--- Unit 7/Session 1/V1 Problem Set/prob5.py
+++ Unit 7/Session 1/V1 Problem Set/prob5.py
@@ -28,3 +28,28 @@ Example Output:
 
 # Expected Output: 5
 # Explanation: 11 has index 5 in the list'''
+
+def binary_search(lst, target):
+	# Initialize a left pointer to the 0th index in the list
+	# Initialize a right pointer to the last index in the list
+	left = 0
+	right = len(lst) -1
+	# While left pointer is less than right pointer:
+		# Find the middle index of the array
+		
+		# If the value at the middle index is the target value:
+			# Return the middle index
+		# Else if the value at the middle index is less than our target value:
+			# Update pointer(s) to only search right half of the list in next loop iteration
+		# Else
+			# Update pointer(s) to only search left half of the list in next loop iteration
+	while left < right:
+		middle = (left + right) //2
+		if lst[middle] == target:
+			return middle
+		elif lst[middle] < target:
+			right = middle -1
+		else:
+			left = middle +1
+	# If we search whole list and haven't found target value, return -1
+	return -1
