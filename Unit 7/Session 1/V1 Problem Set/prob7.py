0ef418bc6e43edf710118fc68ee63ca83e41ab54
Jana Pulak <jpulak@gmail.com>
2026-07-26 01:14:32 -0500

    s1-7

--- Unit 7/Session 1/V1 Problem Set/prob7.py
+++ Unit 7/Session 1/V1 Problem Set/prob7.py
@@ -21,7 +21,13 @@ def find_floor(lst, x):
 		mid = (low+high) //2
 		if lst[mid] <=x:
 			floor = lst[mid]
+			low =mid+1
+		else:
+			high = mid-1
+	return floor
 
-
+lst = [1, 2, 8, 10, 11, 12, 19]
+x = 5
+print(find_floor(lst, x))
 # Expected Output: 1
 # 2 is the largest element in the list that is less than or equal to 5. 2 has index 1 in the list.
