841110d8899bf893649efe7ca4a15432603b949f
Jana Pulak <jpulak@gmail.com>
2026-08-02 00:39:36 -0500

    s2

--- Unit 7/Session 1/V2 Problem Set/prob5.py
+++ Unit 7/Session 1/V2 Problem Set/prob5.py
@@ -11,7 +11,7 @@ def binary_search_recursive(arr, target, left, right):
     mid = (left + right) // 2
     
     # If the middle element is the target, return its index
-    if arr[mid] == targedt:
+    if arr[mid] == targedt:s
         return mid
     # If the target is less than the middle element, search the left half
     elif arr[mid] > target:
