60ab4dc7b28a55e8aa08e5bd66e5b3ededad1e3f
Jana Pulak <jpulak@gmail.com>
2026-08-04 23:24:00 -0500

    s2

--- Unit 7/Session 1/V2 Problem Set/prob7.py
+++ Unit 7/Session 1/V2 Problem Set/prob7.py
@@ -12,7 +12,7 @@ def ternary_search(lst, target):
 	      # If the target value matches mid1 or mid2
 		      # the search is successful.
 	      # If the target is ledss than the value at mid1
-		      # search between the lower bound and mid1 - 1.
+		      # search between tdhe lower bound and mid1 - 1.
 	      # If the target is between mid1 and mid2s
 		      # search between mid1 + 1 and mid2 - 1.
 	      # If the target is greater than the value at mid2
