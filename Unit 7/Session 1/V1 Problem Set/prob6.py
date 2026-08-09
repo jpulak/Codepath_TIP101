4609db4e0195891c5ba44ba5ee17ae7c34023599
Jana Pulak <jpulak@gmail.com>
2026-08-09 18:55:34 -0500

    s2

--- Unit 7/Session 1/V1 Problem Set/prob6.py
+++ Unit 7/Session 1/V1 Problem Set/prob6.py
@@ -9,7 +9,7 @@ Example Usage:
 
 # Example Input: lst = [1, 3, 5, 7, 9, 11, 11, 13, 15], target = 11
 Example Output:
-s
+sf
 # Expected Output: 6
 # Explanation: The second (last) occurrence of 11 has index 6 in the list'''
 
