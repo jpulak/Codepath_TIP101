e9fa9f56c0be475358e3df3d3cdfa5742d175cca
Jana Pulak <jpulak@gmail.com>
2026-08-11 03:40:54 -0500

    s2

--- Unit 7/Session 1/V1 Problem Set/prob3.py
+++ Unit 7/Session 1/V1 Problem Set/prob3.py
@@ -2,7 +2,7 @@
 '''Without using the built-in sum() function, write a function sum_list() that calculates the sum of all values in a list recursively.
 
 What is the time complexity of this function? What is the space complexity?
-
+z
 def sum_list(lst):
 	pass
 Example Usage:
