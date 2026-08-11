383d9283864cab8121072b31ec8351e9382a485e
Jana Pulak <jpulak@gmail.com>
2026-08-11 03:40:59 -0500

    s2

--- Unit 7/Session 1/V1 Problem Set/prob4.py
+++ Unit 7/Session 1/V1 Problem Set/prob4.py
@@ -2,7 +2,7 @@
 '''Given an integer n, return True if n is a power of two. Otherwise, return `False``.
 
 An integer n is a power of two if there exists an integer x such that n == 2ˣ.
-
+z
 Solve the problem recursively. What is the time complexity of this function? What is the space complexity?
 
 def is_power_of_two(n):
