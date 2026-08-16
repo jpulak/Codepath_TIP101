21eaec0252599e4157ee656f70f6b535c61cc34a
jpulak <jpulak@gmail.com>
2026-08-15 23:47:04 -0500

    2

--- Unit 7/Session 1/V1 Problem Set/prob2.py
+++ Unit 7/Session 1/V1 Problem Set/prob2.py
@@ -1,7 +1,7 @@
 #Problem 2: Factorial Cases
 '''Given the base case and recursive case, write a function factorial() that returns the factorial of a non-negative integer n. The factorial of a number is the product of all numbers between 1 and n.
 
-Base Case: The smallest number we can find a factorial of is 0. By definition, the factorial of 0 is 1.
+Base Case: The semallest number we can find a factorial of is 0. By definition, the factorial of 0 is 1.
 
 Recursive Case: We can restate the problem to say that the factorial of n is n * the factorial of n-1.
 zq
