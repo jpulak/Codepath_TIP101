e3abeff952b3ec21ee41c0a2ed07616302dc1bac
jpulak <jpulak@gmail.com>
2026-08-15 23:46:58 -0500

    1

--- Unit 7/Session 1/V1 Problem Set/prob1.py
+++ Unit 7/Session 1/V1 Problem Set/prob1.py
@@ -6,7 +6,7 @@ Step 1: Copy the recursive function repeat_hello() into your IDE and run it.
 Step 2: Then create another function repeat_hello_iterative() that produces the same output without using recursion.
 q
 Compare your iterative (non-recursive) solution to the recursive solution provided. What is similar? What is different?
-fsz
+fsze
 def repeat_hello(n):
 	if n > 0:
 		print("Hello")
