ec8c052d98d85a326996046f1b59f3efdfc57259
jpulak <jpulak@gmail.com>
2026-08-22 21:43:44 -0500

    2

--- Unit 7/Session 1/V1 Problem Set/prob2.py
+++ Unit 7/Session 1/V1 Problem Set/prob2.py
@@ -2,7 +2,7 @@
 '''Given the base case and recursive case, write a function factorial() that returns the factorial of a non-negative integer n. The factorial of a number is the product of all numbers between 1 and n.
 
 Base Case: The semallest number we can find a factorial of is 0. By definition, the factorial of 0 is 1.
-s
+sa
 Recursive Case: We can restate the problem to say that the factorial of n is n * the factorial of n-1.
 zq
 def factorials(n):
