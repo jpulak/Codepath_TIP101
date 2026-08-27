38944880a726edaab7db8d79635637ba28782c19
jpulak <jpulak@gmail.com>
2026-08-26 21:23:28 -0500

    9

--- Unit 7/Session 1/V3 Problem Set/prob2.py
+++ Unit 7/Session 1/V3 Problem Set/prob2.py
@@ -2,7 +2,7 @@
 '''Given the base case and recursive case, write a recursive function string_length() that returns the length of a string s without using the built-in len() function.
 
 Base Case: An empty string should have size 0.
-
+1
 Recursive Case: We can restate the problem to say that the string length is 1 + the length of s[1:].
 
 def string_length(s):
