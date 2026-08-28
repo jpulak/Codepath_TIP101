f4be6f98f60b73d030e78993df2ae272e69985f2
jpulak <jpulak@gmail.com>
2026-08-27 23:13:04 -0500

    x

--- Unit 7/Session 1/V3 Problem Set/prob5.py
+++ Unit 7/Session 1/V3 Problem Set/prob5.py
@@ -1,6 +1,6 @@
 #Problem 5: Binary Search III
 '''Binary search is a searching algorithm that allows us to efficiently find the index of a given value within a sorted list. Given the pseudo code for binary search below, implement an iterative (non-recursive) implementation of binary search that returns True if the given target is in the list and False otherwise. There is also a recursive alternative that we’ll cover in the session 2 problem set!
-
+a
 Evaluate the time and space complexity of your implementation.
 1
 def binary_search(lst, target):
